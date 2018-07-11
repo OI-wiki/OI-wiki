764eefcabf6f2b1b6aee9af8b18d3be678fbfb30
Ir1d <sirius.caffrey@gmail.com>
2018-07-11 11:52:01 +0800

    feat: init

---
+++ docs/_static/js/extra.js
@@ -0,0 +1,29 @@
+window.MathJax = {
+    tex2jax: {
+      inlineMath: [ ["\\(","\\)"] ],
+      displayMath: [ ["\\[","\\]"] ]
+    },
+    TeX: {
+      TagSide: "right",
+      TagIndent: ".8em",
+      MultLineWidth: "85%",
+      equationNumbers: {
+        autoNumber: "AMS",
+      },
+      unicode: {
+        fonts: "STIXGeneral,'Arial Unicode MS'"
+      }
+    },
+    showProcessingMessages: false,
+    messageStyle: "none"
+  };
+window.addEventListener('load', function() { 
+    var p=localStorage.getItem("data-md-color-primary");
+    if (p){
+        document.body.setAttribute('data-md-color-primary',p);
+    }
+    var a=localStorage.getItem('data-md-color-accent');
+    if (a){
+        document.body.setAttribute('data-md-color-accent',a);
+    }
+}, false); 
---
+++ requirements.txt
@@ -0,0 +1,2 @@
+mkdocs
+mkdocs-material
