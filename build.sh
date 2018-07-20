0bd1624f2f124c62c057296cf72c5a5faad0dc5f
Ir1d <sirius.caffrey@gmail.com>
2018-07-20 11:50:13 +0800

    feat: use livere instead

--- build.sh
+++ build.sh
@@ -16,5 +16,6 @@ sed -i "s/name: 'material'/name : null\n  custom_dir: 'mkdocs-material\/material
 # Change Google CDN to loli.net
 sed -i 's/fonts.gstatic.com/gstatic.loli.net/g' mkdocs-material/material/base.html
 sed -i 's/fonts.googleapis.com/fonts.loli.net/g' mkdocs-material/material/base.html
+cp ./static/disqus.html mkdocs-material/material/partials/integrations/disqus.html
 
 mkdocs build
