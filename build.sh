dcf5b8cf0ca35bf43b41cfd6ff8f003b7dd31c96
Ir1d <sirius.caffrey@gmail.com>
2018-08-11 14:37:36 +0800

    feat: use 404 file from mkdocs-material

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ set -e
 if [ ! -d "mkdocs-material" ] ; then
   git clone --depth 1 https://github.com/squidfunk/mkdocs-material.git
 fi
-sed -i "s/name: 'material'/name : null\n  custom_dir: 'mkdocs-material\/material'/g" mkdocs.yml
+sed -i "s/name: 'material'/name : null\n  custom_dir: 'mkdocs-material\/material'\n  static_templates:\n    - 404.html/g" mkdocs.yml
 
 # Change Google CDN to loli.net
 sed -i 's/fonts.gstatic.com/gstatic.loli.net/g' mkdocs-material/material/base.html
