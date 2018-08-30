b96e4d8e110092a0edc0fe577a5628a3a6047804
frank-xjh <frank99-xu@outlook.com>
2018-08-30 15:44:08 +0800

    fix: instantclick

--- build.sh
+++ build.sh
@@ -18,8 +18,9 @@ sed -i "s/name: 'material'/name: null\n  custom_dir: 'mkdocs-material\/material'
 # Change Google CDN to loli.net
 sed -i 's/fonts.gstatic.com/gstatic.loli.net/g' mkdocs-material/material/base.html
 sed -i 's/fonts.googleapis.com/fonts.loli.net/g' mkdocs-material/material/base.html
+sed -i 's/<head>/<head data-no-instant>/g' mkdocs-material/material/base.html
 sed -i 's/{{ page.content }}/{% set pagetime = config.extra.pagetime %} {% if page and page.meta and page.meta.pagetime is string %} {% set pagetime = page.meta.pagetime %} {% endif %}{% if pagetime %}<blockquote class="page-time"><\/blockquote>{% endif %}\n                {{ page.content }}/g' mkdocs-material/material/base.html
 cp ./static/disqus.html mkdocs-material/material/partials/integrations/disqus.html
 cp ./static/footer.html mkdocs-material/material/partials/footer.html
 
-mkdocs serve -v
+mkdocs build -v
