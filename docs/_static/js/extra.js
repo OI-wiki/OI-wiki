f9f16cc4c6307b117135129dc833b008ef707bfd
MingqiHuang <hmq011212@163.com>
2018-09-07 07:24:48 +0800

    remove useless mathjax variables

--- docs/_static/js/extra.js
+++ docs/_static/js/extra.js
@@ -1,28 +1,3 @@
-window.MathJax = {
-  tex2jax: {
-    inlineMath: [['$', '$'], ["\\(", "\\)"]],
-    displayMath: [['$$', '$$'], ["\\[", "\\]"]],
-    processEscapes: true,
-    processEnvironments: true,
-    ignoreClass: ".*|",
-    processClass: "arithmatex"
-  },
-  TeX: {
-    TagSide: "right",
-    TagIndent: ".8em",
-    MultLineWidth: "85%",
-    equationNumbers: {
-      autoNumber: "AMS",
-    },
-    unicode: {
-      fonts: "STIXGeneral,'Arial Unicode MS'"
-    }
-  },
-  showProcessingMessages: false,
-  messageStyle: "none",
-  jax: ["input/TeX", "output/SVG"]
-};	
-
 window.addEventListener('load', function() { 
     var p=localStorage.getItem("data-md-color-primary");
     if (p){
