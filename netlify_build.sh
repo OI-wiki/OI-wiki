13ea3a5bcfb671349af00b66f3b9e07da2c78e07
MingqiHuang <hmq011212@163.com>
2018-09-07 09:52:52 +0800

    fix build

--- netlify_build.sh
+++ netlify_build.sh
@@ -28,7 +28,7 @@ sed -i 's/{{ page.content }}/{% set pagetime = config.extra.pagetime %} {% if pa
 cp ./static/disqus.html mkdocs-material/material/partials/integrations/disqus.html
 cp ./static/footer.html mkdocs-material/material/partials/footer.html
 
-cp ./static/extra.js docs/_static/js/extra.js
+# cp ./static/extra.js docs/_static/js/extra.js
 
 mkdocs build -v
 
