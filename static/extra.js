3b64a1a304f368b720209bb9d0a2d90f07e986c0
Ir1d <sirius.caffrey@gmail.com>
2018-09-11 01:51:47 +0800

    feat: add more info in the bottom of the page

--- static/extra.js
+++ static/extra.js
@@ -1,12 +1,12 @@
-window.addEventListener('load', function() { 
-    var p=localStorage.getItem("data-md-color-primary");
-    if (p){
-        document.body.setAttribute('data-md-color-primary',p);
-    }
-    var a=localStorage.getItem('data-md-color-accent');
-    if (a){
-        document.body.setAttribute('data-md-color-accent',a);
-    }
+window.addEventListener('load', function () {
+  var p = localStorage.getItem("data-md-color-primary");
+  if (p) {
+    document.body.setAttribute('data-md-color-primary', p);
+  }
+  var a = localStorage.getItem('data-md-color-accent');
+  if (a) {
+    document.body.setAttribute('data-md-color-accent', a);
+  }
 }, false);
 
 var cur = document.getElementsByClassName('page-time')[0]
@@ -27,3 +27,41 @@ if (cur) {
 }
 
 document.getElementsByTagName("html")[0].lang = "zh-Hans"; // change language to `zh-Hans` for Han.js.
+
+function foo(response) {
+  var data = response.data;
+  var num = response.data.length;
+  var ti = new Date(data[0].commit.author.date).toLocaleString('zh-CN', { timeZone: "Asia/Shanghai", hour12: false });
+
+  var author_list = [];
+  for (var i = 0; i < num; ++i) {
+    author_list.push(data[i].author.login);
+  }
+  var cnts = author_list.reduce(function (obj, val) {
+    obj[val] = (obj[val] || 0) + 1;
+    return obj;
+  }, {});
+  //Use the keys of the object to get all the values of the array
+  //and sort those keys by their counts
+  var sorted = Object.keys(cnts).sort(function (a, b) {
+    return cnts[b] - cnts[a];
+  });
+  sorted = sorted.map(function(x) {
+    return `<a href=https://github.com/${x}>${x}</a>`
+  })
+  document.getElementsByClassName('facts_modified')[0].innerHTML = (ti);
+  document.getElementsByClassName('page_contributors')[0].innerHTML = (sorted.join(', '));
+}
+
+var site_url = 'https://oi-wiki.org/';
+var url = document.URL.replace(site_url, '').replace(site_url, '').slice(0, -1);
+var script = document.createElement('script');
+if (url == "") {
+  // HomePage
+  document.getElementsByClassName('edit_history')[0].setAttribute('href', `https://github.com/24OI/OI-wiki/commits/master/docs/index.md`);
+  script.src = `https://api.github.com/repos/24OI/OI-WIki/commits?path=docs/index.md&callback=foo`;
+} else {
+  document.getElementsByClassName('edit_history')[0].setAttribute('href', `https://github.com/24OI/OI-wiki/commits/master/docs/${url}.md`);
+  script.src = `https://api.github.com/repos/24OI/OI-WIki/commits?path=docs/${url}.md&callback=foo`;
+}
+document.getElementsByTagName('head')[0].appendChild(script);
