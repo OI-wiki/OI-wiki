4c84f2cd9a1da0f44397c943b0d7da5a31a76c75
Ir1d <sirius.caffrey@gmail.com>
2018-10-01 00:33:45 +0800

    fix: remove bootcss

--- netlify_build.sh
+++ netlify_build.sh
@@ -12,10 +12,10 @@ set -e
 # Clone Theme for Editing
 if [ ! -d "mkdocs-material" ] ; then
   git clone --depth 1 https://github.com/squidfunk/mkdocs-material.git
-  sed -i '9a\<meta http-equiv="x-dns-prefetch-control" content="on">\n\<link rel="dns-prefetch" href="//fonts.loli.net">\n\<link rel="dns-prefetch" href="//cdn.bootcss.com">\n\<link rel="dns-prefetch" href="//unpkg.com">\n\<link rel="dns-prefetch" href="//npmcdn.com">\n\<link rel="dns-prefetch" href="//oi-wiki.org">\n\<link rel="dns-prefetch" href="//api.github.com">' mkdocs-material/material/base.html
+  sed -i '9a\<meta http-equiv="x-dns-prefetch-control" content="on">\n\<link rel="dns-prefetch" href="//fonts.loli.net">\n\<link rel="dns-prefetch" href="//unpkg.com">\n\<link rel="dns-prefetch" href="//oi-wiki.org">\n\<link rel="dns-prefetch" href="//api.github.com">' mkdocs-material/material/base.html
 fi
 sed -i "s/name: 'material'/name: null\n  custom_dir: 'mkdocs-material\/material'\n  static_templates:\n    - 404.html/g" mkdocs.yml
-sed -i "s/- 'https:\/\/cdn.bootcss.com\/mathjax\/2.7.2\/MathJax.js?config=TeX-MML-AM_CHTML'//g" mkdocs.yml 
+sed -i "s/- 'https:\/\/cdn.staticfile.org\/mathjax\/2.7.5\/MathJax.js?config=TeX-MML-AM_CHTML'//g" mkdocs.yml
 
 # Change Google CDN to loli.net
 sed -i 's/fonts.gstatic.com/gstatic.loli.net/g' mkdocs-material/material/base.html
