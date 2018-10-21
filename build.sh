32e738a5a502ae6387121a53642784787cf93d49
frank-xjh <frank99-xu@outlook.com>
2018-10-21 19:02:01 +0800

    upd: async loading

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ set -e
 if [ ! -d "mkdocs-material" ] ; then
   git clone --depth 1 https://github.com/squidfunk/mkdocs-material.git
   sed -i '9a\<meta http-equiv="x-dns-prefetch-control" content="on">\n\<link rel="dns-prefetch" href="//fonts.loli.net">\n\<link rel="dns-prefetch" href="//cdnjs.loli.net">\n\<link rel="dns-prefetch" href="//oi-wiki.org">\n\<link rel="dns-prefetch" href="//cdn.jsdelivr.net">\n\<link rel="dns-prefetch" href="//api.github.com">' mkdocs-material/material/base.html
+  sed -i 's/<script/<script async/g' mkdocs-material/material/base.html
+  sed -i 's/<link/<link media="bogus"/g' mkdocs-material/material/base.html
 fi
 sed -i "s/name: 'material'/name: null\n  custom_dir: 'mkdocs-material\/material'\n  static_templates:\n    - 404.html/g" mkdocs.yml
 sed -i "s/- 'https:\/\/cdnjs.loli.net\/ajax\/libs\/mathjax\/2.7.5\/MathJax.js?config=TeX-MML-AM_CHTML'//g" mkdocs.yml
