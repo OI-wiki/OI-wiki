dce5b6e513e5098437dc156dff13411b73f7973e
Margatroid <351652632@qq.com>
2018-12-26 13:06:47 +0800

    feat: add minify resources, including html, css, js & images

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const htmlmin = require('gulp-htmlmin')
+const imagemin = require('gulp-imagemin')
+const purifycss = require('gulp-purifycss')
+const postcss = require('gulp-postcss')
+const run = require('run-sequence')
+
+gulp.task('minify:html', () => {
+  return gulp.src('site/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyURLs: true,
+      removeComments: true,
+      removeOptionalTags: true,
+      removeRedundantAttributes: true,
+      removeStyleLinkTypeAttributes: true,
+      removeScriptTypeAttributes: true,
+      useShortDoctype: true
+    }))
+    .pipe(gulp.dest('site'))
+})
+
+gulp.task('minify:css', () => {
+  return gulp
+    .src(`site/**/*.css`)
+    .pipe(purifycss([`site/**/*.html`, `site/**/*.js`]))
+    .pipe(
+      postcss([
+        require('cssnano')()
+      ])
+    )
+    .pipe(gulp.dest('site'))
+})
+
+gulp.task('minify:image', () => {
+  return gulp
+    .src(`site/**/*.{png,jpg,svg}`)
+    .pipe(imagemin())
+    .pipe(gulp.dest('site'))
+})
+
+gulp.task('minify', cb => {
+  run('minify:image', ['minify:html', 'minify:css'], cb)
+})
