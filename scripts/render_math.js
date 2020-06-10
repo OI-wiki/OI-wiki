49164b098931b0a94c855943c0f30f19e3332a28
SukkaW <isukkaw@gmail.com>
2020-06-10 22:23:26 +0800

    fix(mathjax): do not promisify mathjax

--- scripts/render_math.js
+++ scripts/render_math.js
@@ -5,26 +5,6 @@ const { green, red, yellow, bgRed } = require('chalk');
 const log = require('fancy-log');
 
 const { mjpage } = require('mathjax-node-page');
-const { promisify } = require('util');
-
-mjpage[promisify.custom] = input => {
-  return new Promise((resolve, reject) => {
-    try {
-      mjpage(
-        input,
-        {
-          format: ["TeX"]
-        },
-        { svg: true, ex: 8, cjkCharWidth: 18, linebreaks: true },
-        resolve
-      );
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
-
-const mathJaxRenderer = promisify(mjpage);
 
 const distDir = join(dirname(__dirname) + '/site');
 
@@ -32,38 +12,44 @@ async function main() {
   const doTask = fn => fn();
 
   const items = await listDir(distDir).map(item => {
-    return async () => {
+    return () => {
       const START_TIME = +new Date();
       if (item.endsWith('.html')) {
         const filename = join(distDir, item);
-        const content = await readFile(filename);
 
-        const preProcesed = content
-          .replace(/<span class="MathJax_Preview">.+?<\/span><script type="math\/tex">/gi, '<script type="math/tex">')
-          .replace(/<div class="MathJax_Preview">[\s\S]*?<\/div>/gi, '');
+        readFile(filename).then(content => {
+          const preProcesed = content
+            .replace(/<span class="MathJax_Preview">.+?<\/span><script type="math\/tex">/gi, '<script type="math/tex">')
+            .replace(/<div class="MathJax_Preview">[\s\S]*?<\/div>/gi, '');
 
-        let result = null;
+          let result = null;
 
-        try {
-          result = await mathJaxRenderer(preProcesed);
-        } catch (e) {
-          log.error(`${bgRed('ERROR')} ${yellow(item)} ${red(`rendered failed`)}, detailed error see below:`);
-          console.error(e);
-        }
+          mjpage(
+            preProcesed,
+            {
+              format: ["TeX"]
+            },
+            { svg: true, ex: 8, cjkCharWidth: 18, linebreaks: true, timeout: 20 * 1000 },
+            output => { result = output; }
+          );
 
-        if (result) {
+          return result;
+        }).then(result => {
           log(`${green('INFO')}  ${yellow(item)} rendered finished (${+new Date() - START_TIME}ms).`);
           return writeFile(filename, result)
-        };
-
-        return;
+        }, e => {
+          throw new Error(e);
+        }).catch(e => {
+          log.error(`${bgRed('ERROR')} ${yellow(item)} ${red(`rendered failed`)}, detailed error see below:`);
+          console.error(e);
+        });
       }
 
       return;
     }
   });
 
-  Promise.all(Promise.map(items, doTask, { concurrency: 10 }));
+  Promise.all(Promise.map(items, doTask, { concurrency: 3 }));
 }
 
 main();
