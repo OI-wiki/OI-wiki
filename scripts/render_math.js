4e58e44a5dbac257af392a4d0d9a92d5aee18ec1
SukkaW <isukkaw@gmail.com>
2020-06-10 21:46:50 +0800

    refactor(mathjax): rewritten mathjax renderer

--- scripts/render_math.js
+++ scripts/render_math.js
@@ -1,26 +1,69 @@
-const mjpage = require("mathjax-node-page").mjpage;
-const fs = require("fs");
+const Promise = require('bluebird');
+const { join, dirname } = require('path');
+const { listDir, readFile, writeFile } = require('hexo-fs');
+const { green, red, yellow, bgRed } = require('chalk');
+const log = require('fancy-log');
 
-filename = process.argv[2];
-try {
-  console.log("Starts rendering: ", filename);
-  let input = fs.readFileSync(filename, "utf-8");
-  mjpage(
-    input
-      .replace(
-        /\<span class=\"MathJax\_Preview\"\>.+?\<\/span\>\<script type="math\/tex"\>/gi,
-        `<script type="math/tex">`
-      )
-      .replace(/\<div class=\"MathJax_Preview\"\>[\s\S]*?\<\/div\>/gi, ""),
-    {
-      format: ["TeX"]
-    },
-    { svg: true, ex: 8, cjkCharWidth: 18, linebreaks: true },
-    function(output) {
-      console.log("Finished rendering: ", filename);
-      fs.writeFileSync(filename, output, "utf-8");
+const { mjpage } = require('mathjax-node-page');
+const { promisify } = require('util');
+
+mjpage[promisify.custom] = input => {
+  return new Promise((resolve, reject) => {
+    try {
+      mjpage(
+        input,
+        {
+          format: ["TeX"]
+        },
+        { svg: true, ex: 8, cjkCharWidth: 18, linebreaks: true },
+        resolve
+      );
+    } catch (e) {
+      reject(e);
     }
-  );
-} catch (e) {
-  console.log(e);
+  });
+};
+
+const mathJaxRenderer = promisify(mjpage);
+
+const distDir = join(dirname(__dirname) + '/site');
+
+async function main() {
+  const doTask = fn => fn();
+
+  const items = await listDir(distDir).map(item => {
+    return async () => {
+      const START_TIME = +new Date();
+      if (item.endsWith('.html')) {
+        const filename = join(distDir, item);
+        const content = await readFile(filename);
+
+        const preProcesed = content
+          .replace(/<span class="MathJax_Preview">.+?<\/span><script type="math\/tex">/gi, '<script type="math/tex">')
+          .replace(/<div class="MathJax_Preview">[\s\S]*?<\/div>/gi, '');
+
+        let result = null;
+
+        try {
+          result = await mathJaxRenderer(preProcesed);
+        } catch (e) {
+          log.error(`${bgRed('ERROR')} ${yellow(item)} ${red(`rendered failed`)}, detailed error see below:`);
+          console.error(e);
+        }
+
+        if (result) {
+          log(`${green('INFO')}  ${yellow(item)} rendered finished (${+new Date() - START_TIME}ms).`);
+          return writeFile(filename, result)
+        };
+
+        return;
+      }
+
+      return;
+    }
+  });
+
+  Promise.all(Promise.map(items, doTask, { concurrency: 10 }));
 }
+
+main();
