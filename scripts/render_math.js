aa056a26a963cc52aa1a019c8e5ca2c67d683dac
SukkaW <isukkaw@gmail.com>
2020-06-10 23:00:15 +0800

    refactor(mathjax): continuing fighting with OOM
    - require mjpage for every file
    - update mjpage to latest
    - set max_old_space_size to 3072

--- scripts/render_math.js
+++ scripts/render_math.js
@@ -2,11 +2,29 @@ const Promise = require('bluebird');
 const { join, dirname } = require('path');
 const { listDir, readFile, writeFile } = require('hexo-fs');
 const { green, red, yellow, bgRed } = require('chalk');
-const log = require('fancy-log');
-
-const { mjpage } = require('mathjax-node-page');
 
 const distDir = join(dirname(__dirname) + '/site');
+const { mjpage } = require('mathjax-node-page');
+const { promisify } = require('util');
+
+mjpage[promisify.custom] = input => {
+  return new Promise((resolve, reject) => {
+    try {
+      mjpage(
+        input,
+        {
+          format: ["TeX"]
+        },
+        { svg: true, ex: 8, cjkCharWidth: 18, linebreaks: true },
+        resolve
+      );
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+const mathJaxRenderer = promisify(mjpage);
 
 async function main() {
   const doTask = fn => fn();
@@ -14,35 +32,25 @@ async function main() {
   const items = await listDir(distDir).map(item => {
     return () => {
       if (item.endsWith('.html')) {
+        let START_TIME, END_TIME;
         const filename = join(distDir, item);
 
         readFile(filename).then(content => {
+          START_TIME = +new Date();
+
           const preProcesed = content
             .replace(/<span class="MathJax_Preview">.+?<\/span><script type="math\/tex">/gi, '<script type="math/tex">')
             .replace(/<div class="MathJax_Preview">[\s\S]*?<\/div>/gi, '');
 
-          const START_TIME = +new Date();
-          let result = null;
-
-          mjpage(
-            preProcesed,
-            {
-              format: ["TeX"]
-            },
-            { svg: true, ex: 8, cjkCharWidth: 18, linebreaks: true, timeout: 20 * 1000 },
-            output => { result = output; }
-          );
-
-          const END_TIME = +new Date();
-
-          return { result, START_TIME, END_TIME };
-        }).then(({ result, START_TIME, END_TIME }) => {
-          log(`${green('INFO')}  ${yellow(item)} rendered finished (${END_TIME - START_TIME}ms).`);
+          return mathJaxRenderer(preProcesed);
+        }).then(result => {
+          END_TIME = +new Date();
+          console.log(`${green('INFO')}  ${yellow(item)} rendered finished (${(END_TIME - START_TIME) / 1000}s).`);
           return writeFile(filename, result);
         }, e => {
           throw new Error(e);
         }).catch(e => {
-          log.error(`${bgRed('ERROR')} ${yellow(item)} ${red(`rendered failed`)}, detailed error see below:`);
+          console.error(`${bgRed('ERROR')} ${yellow(item)} ${red(`rendered failed`)}, detailed error see below:`);
           console.error(e);
         });
       }
