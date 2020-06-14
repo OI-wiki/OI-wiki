4eae04a1c1b91330972edd6457ea8865566e65a2
mgt <mgt@oi-wiki.org>
2020-06-14 15:51:07 +0800

    pref(build): remove mathjax for production build

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,4 +17,4 @@ fi
 git rev-parse --short HEAD | xargs -I % sed -i "s/githash: ''/githash: '%'/g" mkdocs.yml
 sed -i "s/name: 'material'/name: null\n  custom_dir: 'mkdocs-material\/material'\n  static_templates:\n    - 404.html/g" mkdocs.yml
 #Will NOT Use Mathjax for Deploy
-sed -i "s/- 'https:\/\/cdnjs.loli.net\/ajax\/libs\/mathjax\/2.7.5\/MathJax.js?config=TeX-MML-AM_CHTML'//g" mkdocs.yml
+sed -i "s/- 'https:\/\/cdn.jsdelivr.net\/npm\/mathjax@2.7.5\/MathJax.js?config=TeX-MML-AM_CHTML'//g" mkdocs.yml
