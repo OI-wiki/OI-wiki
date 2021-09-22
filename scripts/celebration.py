91438c1dc45482e91320754dfa371fb8647ba642
Yufan You <ouuansteve@gmail.com>
2021-09-22 14:28:06 +0800

    ci: automatically create star celebrations

---
+++ scripts/celebration.py
@@ -0,0 +1,82 @@
+import requests
+import json
+import os
+
+headers = {
+    "Authorization": f'token {os.environ["GITHUB_TOKEN"]}'
+}
+
+label = "Celebration / 庆贺"
+
+query = f'''
+query {{
+    repository(owner: "OI-wiki", name: "OI-wiki") {{
+        id
+        stargazerCount
+        label(name: "{label}") {{
+            id
+        }}
+        issues(labels: "{label}", first: 100) {{
+            nodes {{
+                number
+                title
+            }}
+        }}
+        defaultBranchRef {{
+            target {{
+                ... on Commit {{
+                    history {{
+                        totalCount
+                    }}
+                }}
+            }}
+        }}
+    }}
+}}
+'''
+
+res = requests.post('https://api.github.com/graphql', json.dumps({'query': query}), headers = headers).json()['data']['repository']
+
+id = res['id']
+stars = res['stargazerCount']
+issues = res['issues']['nodes']
+numbers = []
+
+maxCelebration = 0
+for issue in issues:
+    numbers.append('#' + str(issue['number']))
+    maxCelebration = max(maxCelebration, int(issue['title'].split(' ')[0]))
+
+celebration = maxCelebration * 2
+
+if stars >= celebration - 1: # "will soon" reach
+    mutation = f'''
+mutation {{
+  createIssue(input: {{
+    repositoryId: "{id}",
+    title: "{celebration} Stars Celebration",
+    labelIds: ["{res['label']['id']}"]
+    body: {json.dumps(f"""> To celebrate that our project's stars will soon reach {celebration}.
+> This issue will be closed in one week.
+
+为庆祝我们的 **OI Wiki** 项目 Stars 即将到达 {celebration}，特开此 Issue。
+
+诸位维护本项目也都辛苦了，我刚刚看了下 Commits 也到达了 {res['defaultBranchRef']['target']['history']['totalCount']}，大家在本 Issue 里面想说什么就说什么吧（But please don't spam in this issue）。
+
+祝大家愉快。
+
+<!--（文案来自 @K-Guan）-->
+
+***
+
+非常感谢这么长时间以来大家的支持，我们再接再厉～
+
+最后，祝大家在这特殊的时期身体健康，诸事顺遂。
+
+cc {" ".join(numbers)}
+""", ensure_ascii = False)}
+  }}) {{
+  }}
+}}
+'''
+    requests.post('https://api.github.com/graphql', json.dumps({ 'query': mutation }), headers = headers)
