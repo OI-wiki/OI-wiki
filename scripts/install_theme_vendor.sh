70c4b4c12a98b43db058c23d48c2265dd0204dc2
Menci <huanghaorui301@gmail.com>
2021-12-20 17:56:45 +0800

    Make install_theme_vendor.sh print messages and exit on error

--- scripts/install_theme_vendor.sh
+++ scripts/install_theme_vendor.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash -e
-
 VENDOR_DIR="mkdocs-material/material/assets/vendor"
 rm -rf "$VENDOR_DIR"
 
 # MathJax
 TEMP_DIR="$(mktemp -d -t "download-mathjax-XXXXXXXX")"
 MATHJAX_URL=${MATHJAX_URL:="https://registry.npmjs.org/mathjax/-/mathjax-2.7.5.tgz"}
+echo "Downloading MathJax to $TEMP_DIR"
+echo "URL: $MATHJAX_URL"
 MATHJAX_REQUIRED_FILES=(
 	"config/TeX-MML-AM_CHTML.js"
 	"extensions/TeX"
@@ -26,6 +26,8 @@ rm -rf "$TEMP_DIR"
 # Material Icons
 TEMP_DIR="$(mktemp -d -t "download-material-icons-XXXXXXXX")"
 MATERIAL_ICONS_URL=${MATERIAL_ICONS_URL:="https://registry.npmjs.org/material-icons/-/material-icons-0.2.3.tgz"}
+echo "Downloading material-icons to $TEMP_DIR"
+echo "URL: $MATERIAL_ICONS_URL"
 MATERIAL_ICONS_REQUIRED_FILES=(
 	"iconfont/material-icons.css"
 	"iconfont/MaterialIcons-Regular.woff"
@@ -42,6 +44,8 @@ rm -rf "$TEMP_DIR"
 
 # Gitalk
 TEMP_DIR="$(mktemp -d -t "download-gittalk-XXXXXXXX")"
+echo "Downloading Gitalk to $TEMP_DIR"
+echo "Repo: $TEMP_DIR"
 GITALK_REPO=${GITALK_REPO:="git@github.com:OI-wiki/gitalk.git"}
 GITALK_BRANCH="mv1.0"
 GITALK_REQUIRED_FILES=(
