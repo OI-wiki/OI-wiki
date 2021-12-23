b43f91000187dbf72f0f5613abd99f7e45d0ba93
Menci <huanghaorui301@gmail.com>
2021-12-24 00:25:29 +0800

    Update scripts/install_theme.sh

--- scripts/install_theme.sh
+++ scripts/install_theme.sh
@@ -4,7 +4,7 @@ set -e
 
 THEME_NAME="mkdocs-material"
 if [ ! -d "$THEME_NAME" ] ; then
-  git clone --depth=1 ${THEME_REPO:-"https://github.com/Menci/mkdocs-material.git"} "$THEME_NAME"
+  git clone --depth=1 ${THEME_REPO:-"https://github.com/OI-wiki/mkdocs-material.git"} "$THEME_NAME"
 fi
 
 bash scripts/install_theme_vendor.sh
