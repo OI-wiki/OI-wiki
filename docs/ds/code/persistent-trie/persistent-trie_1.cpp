f38088f9cbb3a9c655b6d67e3ae5d99c97a99ed3
代建杉 <wood3s@foxmail.com>
2022-06-23 18:03:49 +0800

    Update persistent-trie_1.cpp

--- docs/ds/code/persistent-trie/persistent-trie_1.cpp
+++ docs/ds/code/persistent-trie/persistent-trie_1.cpp
@@ -63,7 +63,7 @@ int main() {
       r--;
       if (l == 0)
         printf("%d\n", max(s[n] ^ x,
-                           st.query(st.rt[r], st.rt[max(l - 1, 0)], s[n] ^ x)));
+                           st.query(st.rt[r], st.rt[0], s[n] ^ x)));
       else
         printf("%d\n", st.query(st.rt[r], st.rt[l - 1], s[n] ^ x));
     }
