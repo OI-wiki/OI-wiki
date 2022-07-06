9b13b0d4ef37fd78c18817453983c5c1bf3f1976
Menci <huanghaorui301@gmail.com>
2022-07-06 10:15:27 +0800

    Change authros.json URL back

--- scripts/render_git_history_info.sh
+++ scripts/render_git_history_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-wget https://raw.githubusercontent.com/Menci/OI-wiki/authors-cache/authors.json
+wget https://raw.githubusercontent.com/OI-wiki/OI-wiki/authors-cache/authors.json
 
 node scripts/fetch-authors.js # Update authors cache (incrementally)
 node scripts/html-postprocess.js site
