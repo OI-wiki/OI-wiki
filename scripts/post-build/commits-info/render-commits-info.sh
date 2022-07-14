10d868857d807da321c2f4ce0ab29590adaafa8a
mgt <mgt@oi-wiki.org>
2022-07-14 16:23:49 +0800

    try increase stack size

--- scripts/post-build/commits-info/render-commits-info.sh
+++ scripts/post-build/commits-info/render-commits-info.sh
@@ -7,5 +7,6 @@ DIRNAME="$(dirname -- "${BASH_SOURCE[0]}")"
 
 wget "https://raw.githubusercontent.com/$REPO/authors-cache/authors.json" -O "authors.json"
 
+export NODE_OPTIONS='--max-old-space-size=3049'
 yarn ts-node-esm "$DIRNAME/update-authors-cache.ts" # Update authors cache (incrementally)
 yarn ts-node-esm "$DIRNAME/render-commits-info.ts" site
