38f997226b95d3beda4e30a973da227a7de82958
Menci <huanghaorui301@gmail.com>
2022-07-14 22:12:30 +0800

    refactor: migrate MathJax 2 to MathJax 3

--- scripts/pre-build/pre-build.sh
+++ scripts/pre-build/pre-build.sh
@@ -7,5 +7,3 @@ DIRNAME="$(dirname -- "${BASH_SOURCE[0]}")"
 "$DIRNAME"/install-theme.sh
 
 git rev-parse --short HEAD | xargs -I % sed -i "s/githash: ''/githash: '%'/g" mkdocs.yml
-
-sed -i "s#  - 'assets/vendor/mathjax/MathJax.js?config=TeX-MML-AM_CHTML'##g" mkdocs.yml
