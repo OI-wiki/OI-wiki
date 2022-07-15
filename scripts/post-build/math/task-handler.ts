255a6fadada018423cc2dabaa7648d0e0735e7a3
Shuzhou Liu <64518651+shuzhouliu@users.noreply.github.com>
2022-07-15 13:47:54 +0800

    fix: make prettier happy

--- scripts/post-build/math/task-handler.ts
+++ scripts/post-build/math/task-handler.ts
@@ -6,7 +6,10 @@ import { HTMLElement } from "node-html-parser";
 import { mathjax } from "mathjax-full/js/mathjax.js";
 import { TeX } from "mathjax-full/js/input/tex.js";
 import { CHTML } from "mathjax-full/js/output/chtml.js";
-import { LiteAdaptor, liteAdaptor } from "mathjax-full/js/adaptors/liteAdaptor.js";
+import {
+  LiteAdaptor,
+  liteAdaptor,
+} from "mathjax-full/js/adaptors/liteAdaptor.js";
 import { HTMLHandler } from "mathjax-full/js/handlers/html/HTMLHandler.js";
 import { SafeHandler } from "mathjax-full/js/ui/safe/SafeHandler.js";
 import type { MathDocument } from "mathjax-full/js/core/MathDocument.js";
@@ -16,7 +19,14 @@ import type { LiteElement } from "mathjax-full/js/adaptors/lite/Element.js";
 import { TaskHandler, log } from "../html-postprocess.js";
 
 // Define the MathJax packages to load
-const MATHJAX_PACKAGES = ["ams", "boldsymbol", "colorv2", "html", "noundefined", "physics"];
+const MATHJAX_PACKAGES = [
+  "ams",
+  "boldsymbol",
+  "colorv2",
+  "html",
+  "noundefined",
+  "physics",
+];
 
 // All HTML files will reference the CSS file with relative paths (to the HTML file)
 // The CSS file will reference the fonts files with relative paths (to the CSS file)
@@ -28,7 +38,9 @@ const MATHJAX_TARGET_FONTS_DIR = "assets/fonts/mathjax";
 const MATH_CSR_SCRIPT_SUFFIX = "?math-csr";
 
 // The directory of package `mathjax-full`
-const MATHJAX_SOURCE = path.dirname(module.createRequire(import.meta.url).resolve("mathjax-full/package.json"));
+const MATHJAX_SOURCE = path.dirname(
+  module.createRequire(import.meta.url).resolve("mathjax-full/package.json")
+);
 
 // Add horizontal scrollbar automatically on narrow screens
 const MATHJAX_EXTRA_CSS = `
@@ -48,10 +60,12 @@ export class MathRenderer {
     // Load MathJax's TeX packages
     const packagesDir = path.join(MATHJAX_SOURCE, "js/input/tex");
     await Promise.all(
-      MATHJAX_PACKAGES.map(async packageName => {
+      MATHJAX_PACKAGES.map(async (packageName) => {
         const packageDir = path.join(packagesDir, packageName);
         const packageDirFiles = await fs.promises.readdir(packageDir);
-        const packageConfigurationFile = packageDirFiles.find(filename => filename.endsWith("Configuration.js"));
+        const packageConfigurationFile = packageDirFiles.find((filename) =>
+          filename.endsWith("Configuration.js")
+        );
         return await import(path.join(packageDir, packageConfigurationFile));
       })
     );
@@ -59,23 +73,26 @@ export class MathRenderer {
     this.adaptor = liteAdaptor();
     const inputJax = new TeX({
       packages: {
-        "[+]": MATHJAX_PACKAGES
+        "[+]": MATHJAX_PACKAGES,
       },
       macros: {
         shl: "{\\space\\var|<<|\\space}",
-        shr: "{\\space\\var|>>|\\space}"
+        shr: "{\\space\\var|>>|\\space}",
       },
     });
     const outputJax = new CHTML<LiteElement, unknown, LiteDocument>({
-      fontURL: path.relative(path.dirname(MATHJAX_TARGET_CSS_FILE), MATHJAX_TARGET_FONTS_DIR),
-      adaptiveCSS: false
+      fontURL: path.relative(
+        path.dirname(MATHJAX_TARGET_CSS_FILE),
+        MATHJAX_TARGET_FONTS_DIR
+      ),
+      adaptiveCSS: false,
     });
 
     mathjax.handlers.register(SafeHandler(new HTMLHandler(this.adaptor)));
 
     this.document = mathjax.document("", {
       InputJax: inputJax,
-      OutputJax: outputJax
+      OutputJax: outputJax,
     });
 
     this.document.updateDocument();
@@ -87,7 +104,9 @@ export class MathRenderer {
   }
 
   render(math: string, isDisplay: boolean) {
-    const element = this.document.convert(math, { display: isDisplay }) as LiteElement;
+    const element = this.document.convert(math, {
+      display: isDisplay,
+    }) as LiteElement;
     this.adaptor.setAttribute(element, "title", math);
     return this.adaptor.outerHTML(element);
   }
@@ -97,15 +116,21 @@ export const taskHandler = new (class implements TaskHandler<void> {
   // Emit fonts and CSS file
   async globalInitialize(siteDir: string) {
     log("Copying MathJax fonts");
-    const fontsSourceDir = path.join(MATHJAX_SOURCE, "es5/output/chtml/fonts/woff-v2");
+    const fontsSourceDir = path.join(
+      MATHJAX_SOURCE,
+      "es5/output/chtml/fonts/woff-v2"
+    );
     const fontsDestDir = path.join(siteDir, MATHJAX_TARGET_FONTS_DIR);
 
     const fontFilesToCopy = await fs.promises.readdir(fontsSourceDir);
 
     await fs.promises.mkdir(fontsDestDir, { recursive: true });
     await Promise.all(
-      fontFilesToCopy.map(filename =>
-        fs.promises.copyFile(path.join(fontsSourceDir, filename), path.join(fontsDestDir, filename))
+      fontFilesToCopy.map((filename) =>
+        fs.promises.copyFile(
+          path.join(fontsSourceDir, filename),
+          path.join(fontsDestDir, filename)
+        )
       )
     );
 
@@ -128,8 +153,10 @@ export const taskHandler = new (class implements TaskHandler<void> {
   }
 
   async process(document: HTMLElement, filePath: string) {
-    const mathElements = document.querySelectorAll("div.arithmatex, span.arithmatex");
-    mathElements.map(element => {
+    const mathElements = document.querySelectorAll(
+      "div.arithmatex, span.arithmatex"
+    );
+    mathElements.map((element) => {
       // MKdocs outputs "\(xxxxxxx\)", so we need to remove the border
       const texCode = element.textContent.slice(2, -2);
       const isDisplay = element.tagName === "DIV";
@@ -141,16 +168,24 @@ export const taskHandler = new (class implements TaskHandler<void> {
     // Inject CSS <link> element (if rendered any math elements)
     if (mathElements.length > 0) {
       const htmlFilePathToRoot = path.relative(this.siteDir, filePath);
-      const cssFilePathToHtml = path.relative(htmlFilePathToRoot, MATHJAX_TARGET_CSS_FILE);
+      const cssFilePathToHtml = path.relative(
+        htmlFilePathToRoot,
+        MATHJAX_TARGET_CSS_FILE
+      );
       document
         .querySelector("head")
-        .insertAdjacentHTML("beforeend", `<link rel="stylesheet" href="${cssFilePathToHtml}">`);
+        .insertAdjacentHTML(
+          "beforeend",
+          `<link rel="stylesheet" href="${cssFilePathToHtml}">`
+        );
     }
 
     // Remove client-side rendering script
     document
       .querySelectorAll("script")
-      .filter(element => element.getAttribute("src")?.endsWith(MATH_CSR_SCRIPT_SUFFIX))
-      .forEach(element => element.remove());
+      .filter((element) =>
+        element.getAttribute("src")?.endsWith(MATH_CSR_SCRIPT_SUFFIX)
+      )
+      .forEach((element) => element.remove());
   }
 })();
