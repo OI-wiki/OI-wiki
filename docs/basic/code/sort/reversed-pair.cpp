d1157a07197fc261f99dc716034002f80ae49c96
小蛙 <49180735+littlefrogfromthenorth@users.noreply.github.com>
2022-07-16 21:53:08 +0800

    update.

--- docs/basic/code/sort/reversed-pair.cpp
+++ docs/basic/code/sort/reversed-pair.cpp
@@ -1,33 +1,52 @@
-int ans = 0;
-int a[100005], c[100005];
+#include <bits/stdc++.h>
+using namespace std;
+long long ans = 0;
+int n;
+int a[900009], t[900009];
 
-void merge(int b, int e) {
-  if (b == e) {
+void merge(int ll, int rr) {
+  if (ll == rr) {
     return;
   }
-  int mid = (b + e) / 2, i = b, j = mid + 1, k = b;
-  merge(b, mid);
-  merge(mid + 1, e);
-  while (i <= mid && j <= e) {
+  int mid = (ll + rr) / 2, i = ll, j = mid + 1, k = ll;
+  merge(ll, mid);
+  merge(mid + 1, rr);
+  while (i <= mid && j <= rr) {
     if (a[i] <= a[j]) {
-      c[k++] = a[i++];
+      t[k++] = a[i++];
     } else {
+      /*
+      若要获得本轮产生的所有逆序对, 请去除此条注释.
       cout << a[j] << ':';
       for (int s = i; s <= mid; ++s) {
         cout << a[s] << ' ';
       }
-      cout << endl;
-      c[k++] = a[j++];
+      cout << endl;*/
+      t[k++] = a[j++];
       ans += mid - i + 1;
     }
   }
   while (i <= mid) {
-    c[k++] = a[i++];
+    t[k++] = a[i++];
   }
-  while (j <= e) {
-    c[k++] = a[j++];
+  while (j <= rr) {
+    t[k++] = a[j++];
   }
-  for (int l = b; l <= e; l++) {
-    a[l] = c[l];
+  for (int l = ll; l <= rr; l++) {
+    a[l] = t[l];
   }
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  cin >> n;
+  for (int i = 1; i <= n; ++i) {
+    cin >> a[i];
+  }
+
+  merge(1, n);
+
+  cout << ans << endl;
 }
