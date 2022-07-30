cc6ed4e085beca9b7fdb665de0b6fcfc0f5e45fa
llh721113 <llh721113@outlook.com>
2022-07-30 21:35:24 +0800

    fix: use mjs for ESM

--- scripts/render_git_history_info.sh
+++ scripts/render_git_history_info.sh
@@ -2,5 +2,5 @@
 
 wget https://raw.githubusercontent.com/OI-wiki/OI-wiki/authors-cache/authors.json
 
-node scripts/fetch-authors.cjs # Update authors cache (incrementally)
-node scripts/html-postprocess.cjs site
+node scripts/fetch-authors.js # Update authors cache (incrementally)
+node scripts/html-postprocess.js site
