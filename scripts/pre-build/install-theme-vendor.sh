a471c8684d00d839bfd275529aea6c0dbaec8379
Menci <huanghaorui301@gmail.com>
2022-12-09 01:00:00 +0800

    refactor: upgrade to new version of theme

--- scripts/pre-build/install-theme-vendor.sh
+++ scripts/pre-build/install-theme-vendor.sh
@@ -27,22 +27,3 @@ for FILE in "${MATHJAX_REQUIRED_FILES[@]}"; do
 	cp -r "$FILE_SOURCE_PATH" "$FILE_TARGET_PATH"
 done
 rm -rf "$TEMP_DIR"
-
-# Material Icons
-TEMP_DIR="$(mktemp -d -t "download-material-icons-XXXXXXXX")"
-MATERIAL_ICONS_URL=${MATERIAL_ICONS_URL:-"https://registry.npmjs.org/material-icons/-/material-icons-0.2.3.tgz"}
-echo "Downloading material-icons to $TEMP_DIR"
-echo "URL: $MATERIAL_ICONS_URL"
-MATERIAL_ICONS_REQUIRED_FILES=(
-	"iconfont/material-icons.css"
-	"iconfont/MaterialIcons-Regular.woff"
-	"iconfont/MaterialIcons-Regular.woff2"
-)
-curl "$MATERIAL_ICONS_URL" | tar -C "$TEMP_DIR" -xzf -
-for FILE in "${MATERIAL_ICONS_REQUIRED_FILES[@]}"; do
-	FILE_TARGET_PATH="$VENDOR_DIR/material-icons/$FILE"
-	FILE_SOURCE_PATH="$TEMP_DIR/package/$FILE"
-	mkdir -p "$(dirname "$FILE_TARGET_PATH")"
-	cp -r "$FILE_SOURCE_PATH" "$FILE_TARGET_PATH"
-done
-rm -rf "$TEMP_DIR"
