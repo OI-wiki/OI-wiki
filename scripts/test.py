1b8a2695bfdcb3e9b5e1e67e235239dfa558e099
yusancky <umbrellacky@qq.com>
2023-01-16 14:59:44 +0800

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -10,7 +10,7 @@ def generate_annotations_and_exit(file, message):
 
 
 def test(cppname):
-    name = cppname[:-5]
+    name = cppname[:-4]
     num = name.rfind('/')
     content = name[:num]
     filename = name[num:]
