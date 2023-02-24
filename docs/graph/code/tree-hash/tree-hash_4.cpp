173c226ef1a65211e228929e7bc0eb4c33f42d3c
jifbt <55904367+jifbt@users.noreply.github.com>
2023-02-24 20:00:15 +0800

    feat(graph/code/tree-hash/tree-hash_4.cpp): 格式化代码

--- docs/graph/code/tree-hash/tree-hash_4.cpp
+++ docs/graph/code/tree-hash/tree-hash_4.cpp
@@ -1,43 +1,53 @@
-#include<cstdio>
-#include<cctype>
-#include<vector>
-#include<set>
-#include<random>
-#include<chrono>
-using namespace std;
+#include <cstdio>
+#include <cctype>
+#include <vector>
+#include <set>
+#include <random>
+#include <chrono>
+
 typedef unsigned long long ull;
-const ull mask=chrono::steady_clock::now().time_since_epoch().count();
-ull f(ull x){
-	x^=mask;
-	x^=x<<13;
-	x^=x>>7;
-	x^=x<<17;
-	x^=mask;
-	return x;
-}
-const int N=1e6+10;
-int n;
-ull h[N];
-vector<int>e[N];
-set<ull>s;
-void dfs(int x,int p){
-	h[x]=1;
-	for(int i:e[x]){
-		if(i==p)continue;
-		dfs(i,x);
-		h[x]+=f(h[i]);
-	}
-	s.insert(h[x]);
-}
-int main(){
-	scanf("%d",&n);
-	for(int i=1;i<n;++i){
-		int u,v;
-		scanf("%d%d",&u,&v);
-		e[u].push_back(v);
-		e[v].push_back(u);
-	}
-	dfs(1,0);
-	printf("%lu",s.size());
+
+const ull mask=std::chrono::steady_clock::now().time_since_epoch().count();
+
+ull shift(ull x)
+{
+    x ^= mask;
+    x ^= x << 13;
+    x ^= x >> 7;
+    x ^= x << 17;
+    x ^= mask;
+    return x;
 }
 
+const int N=1e6+10;
+
+int n;
+ull hash[N];
+std::vector<int> edge[N];
+std::set<ull> trees;
+
+void dfs(int x, int p)
+{ 
+    hash[x] = mask ^ 1;
+    for (int i : edge[x]) {
+        if (i == p) {
+            continue;
+        }
+        dfs(i, x);
+        hash[x] += shift(hash[i]);
+    }
+    trees.insert(hash[x]);
+}
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i=1; i<n; ++i) {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        edge[u].push_back(v);
+        edge[v].push_back(u);
+    }
+    dfs(1, 0);
+    printf("%lu", trees.size());
+}
