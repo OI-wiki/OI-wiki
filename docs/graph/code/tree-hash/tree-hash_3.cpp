c4c12af38729622fc2ac3a5a0d17aa0834abeda5
jifbt <55904367+jifbt@users.noreply.github.com>
2023-02-24 20:05:21 +0800

    feat(docs/graph/code/tree-hash/tree-hash_3.cpp): 格式化代码

--- docs/graph/code/tree-hash/tree-hash_3.cpp
+++ docs/graph/code/tree-hash/tree-hash_3.cpp
@@ -12,59 +12,59 @@ const int N=1e5+10, M=998244353;
 const ull mask=std::chrono::steady_clock::now().time_since_epoch().count();
 
 struct Tree {
-	ull hash;
-	ll deg, ans;
-	std::map<ull, ll> son;
-	
-	Tree() {clear();}
-	void add(Tree& o);
-	void remove(Tree& o);
-	void clear();
+    ull hash;
+    ll deg, ans;
+    std::map<ull, ll> son;
+    
+    Tree() {clear();}
+    void add(Tree& o);
+    void remove(Tree& o);
+    void clear();
 };
 
 ll inv(ll x)
 {
-	ll y=M-2, z=1;
-	while (y) {
-		if (y & 1) {
-			z = z * x % M;
-		}
-		x = x * x % M;
-		y >>= 1;
-	}
-	return z;
+    ll y=M-2, z=1;
+    while (y) {
+        if (y & 1) {
+            z = z * x % M;
+        }
+        x = x * x % M;
+        y >>= 1;
+    }
+    return z;
 }
 
 ull shift(ull x)
 {
-	x ^= mask;
-	x ^= x << 13;
-	x ^= x >> 7;
-	x ^= x << 17;
-	x ^= mask;
-	return x;
+    x ^= mask;
+    x ^= x << 13;
+    x ^= x >> 7;
+    x ^= x << 17;
+    x ^= mask;
+    return x;
 }
 
 void Tree::add(Tree& o)
 {
-	ull temp = shift(o.hash);
-	hash += temp;
-	ans = ans * ++deg % M * inv(++son[temp]) % M * o.ans % M;
+    ull temp = shift(o.hash);
+    hash += temp;
+    ans = ans * ++deg % M * inv(++son[temp]) % M * o.ans % M;
 }
-	
+    
 void Tree::remove(Tree& o)
 {
-	ull temp = shift(o.hash);
-	hash -= temp;
-	ans = ans * inv(deg--) % M * son[temp]-- % M * inv(o.ans) % M;
+    ull temp = shift(o.hash);
+    hash -= temp;
+    ans = ans * inv(deg--) % M * son[temp]-- % M * inv(o.ans) % M;
 }
 
 void Tree::clear()
 {
-	hash = mask ^ 1;
-	deg = 0;
-	ans = 1;
-	son.clear();
+    hash = mask ^ 1;
+    deg = 0;
+    ans = 1;
+    son.clear();
 }
 
 std::vector<int> edge[N];
@@ -73,55 +73,55 @@ std::map<ull, ll> trees;
 
 void dfs(int x, int fa)
 {
-	for (int i : edge[x]) {
-		if (i == fa) {
-			continue;
-		}
-		dfs(i, x);
-		sub[x].add(sub[i]);
-	}
+    for (int i : edge[x]) {
+        if (i == fa) {
+            continue;
+        }
+        dfs(i, x);
+        sub[x].add(sub[i]);
+    }
 }
 
 void dfs2(int x, int fa)
 {
-	for (int i : edge[x]) {
-		if (i == fa) {
-			continue;
-		}
-		root[x].remove(sub[i]);
-		root[i] = sub[i];
-		root[i].add(root[x]);
-		root[x].add(sub[i]);
-		dfs2(i, x);
-	}
-	trees[root[x].hash] = root[x].ans;
+    for (int i : edge[x]) {
+        if (i == fa) {
+            continue;
+        }
+        root[x].remove(sub[i]);
+        root[i] = sub[i];
+        root[i].add(root[x]);
+        root[x].add(sub[i]);
+        dfs2(i, x);
+    }
+    trees[root[x].hash] = root[x].ans;
 }
 
 int main()
 {
-	int t, n;
-	scanf("%d", &t);
-	while (t--) {
-		scanf("%d", &n);
-		for (int i=1; i<n; i++) {
-			int u, v;
-			scanf("%d%d", &u, &v);
-			edge[u].push_back(v);
-			edge[v].push_back(u);
-		}
-		dfs(1, 0);
-		root[1] = sub[1];
-		dfs2(1, 0);
-		ll tot = 0;
-		for (auto p : trees) {
-			tot = (tot + p.second) % M;
-		}
-		printf("%lld\n", tot);
-		for (int i=1; i<=n; i++) {
-			edge[i].clear();
-			sub[i].clear();
-			root[i].clear();
-		}
-		trees.clear();
-	}
+    int t, n;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        for (int i=1; i<n; i++) {
+            int u, v;
+            scanf("%d%d", &u, &v);
+            edge[u].push_back(v);
+            edge[v].push_back(u);
+        }
+        dfs(1, 0);
+        root[1] = sub[1];
+        dfs2(1, 0);
+        ll tot = 0;
+        for (auto p : trees) {
+            tot = (tot + p.second) % M;
+        }
+        printf("%lld\n", tot);
+        for (int i=1; i<=n; i++) {
+            edge[i].clear();
+            sub[i].clear();
+            root[i].clear();
+        }
+        trees.clear();
+    }
 }
