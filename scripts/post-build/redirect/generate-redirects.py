6ecbbd6319d19b1fe448ee2e173c3dfd8daa3f25
Yufan You <ouuansteve@gmail.com>
2023-09-21 10:46:13 +0800

    ci: check that _redirects contain all old links (#5149)
    ci: check that all old links are accessible in the new site

--- scripts/post-build/redirect/generate-redirects.py
+++ scripts/post-build/redirect/generate-redirects.py
@@ -2,11 +2,11 @@ import os
 f = open('site/_redirects')
 nginx = open('site/redirect-nginx.conf', 'w')
 for cur in f.readlines():
-    s, t = cur.strip().split(' ')
+    s, t = cur.strip().split()
     # s = s.rstrip('/') + '(\/?(index\.html)?)'
     # t = 'https://oi-wiki.org' + t + ';'
     # nginx.write('~' + s + ' ' + t + "\n")
-    html_path = os.path.join('site', s.lstrip('/'), 'index.html')
+    html_path = os.path.join('site', s.strip('/'), 'index.html')
     os.makedirs(os.path.dirname(html_path), exist_ok=True)
     with open(html_path, 'w') as html_file:
         html_file.write(f'<!DOCTYPE html><html><head><link rel="canonical" href="https://oi-wiki.org{t}"/><meta name="robots" content="noindex"><meta charset="utf-8" /><meta http-equiv="refresh" content="0; url={t}" /></head></html>')
