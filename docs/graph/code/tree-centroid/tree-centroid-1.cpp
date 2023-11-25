c3f50265e0b9fa5018e288c0667e7fc20fe3331c
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2023-11-25 11:05:08 +0000

    fix(tree-centroid-1.cpp): remove extra `\n`

--- docs/graph/code/tree-centroid/tree-centroid-1.cpp
+++ docs/graph/code/tree-centroid/tree-centroid-1.cpp
@@ -6,9 +6,8 @@ const int N = 3e5 + 5;
 int n, q;  // 点数，询问数
 int fa[N];
 vector<int> son[N];
-int siz[N],  // 子树大小
-    ans[N],  // 以节点 u 为根的子树重心是 ans[u]
-
+int siz[N],     // 子树大小
+    ans[N],     // 以节点 u 为根的子树重心是 ans[u]
     weight[N];  // 节点重量
 
 void dfs(int u) {
