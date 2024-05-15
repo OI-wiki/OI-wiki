e8cfbb67fdcbdc3797f22d5c05b77cf403c3d913
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-05-15 06:33:59 +0000

    refactor(sqrt-decomposition_2.cpp): more readable

--- docs/math/code/sqrt-decomposition/sqrt-decomposition_2.cpp
+++ docs/math/code/sqrt-decomposition/sqrt-decomposition_2.cpp
@@ -1,13 +1,18 @@
-#include <stdio.h>
-
 #include <algorithm>
-#define N 100009
+#include <iostream>
+
+constexpr int N = 100009;
 int n, a[N], maxn;
 long long ans[N];
 
-main() {
-  scanf("%d", &n);
-  for (int i = 1; i <= n; scanf("%d", a + i), maxn = std::max(maxn, a[i]), ++i);
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cin >> n;
+  for (int i = 1; i <= n; ++i) {
+    std::cin >> a[i];
+    maxn = std::max(maxn, a[i]);
+  }
   for (int i = 0; i < n; ++i)
     for (int l = 1, r;; l = r + 1) {
       r = std::min(l < a[i] ? (a[i] - 1) / ((a[i] - 1) / l) : N,
@@ -18,5 +23,6 @@ main() {
       if (x > 0) ans[l] += x, ans[r + 1] -= x;  // 累加贡献
     }
   ++ans[0];  // ⌈a/l⌉=(a-1)/l+1的式子当a=0时不成立，需要修正
-  for (int i = 1; i <= maxn; printf("%lld ", ans[i] += ans[i - 1]), ++i);
+  for (int i = 1; i <= maxn; ++i) std::cout << (ans[i] += ans[i - 1]) << " \n"[i == maxn];
+  return 0;
 }
