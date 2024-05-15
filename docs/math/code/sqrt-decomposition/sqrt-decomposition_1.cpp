fb4886843e5f6d4e149e2bfc7819bc90e9ab6971
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-05-15 06:29:14 +0000

    refactor(sqrt-decomposition_1.cpp): more readable

--- docs/math/code/sqrt-decomposition/sqrt-decomposition_1.cpp
+++ docs/math/code/sqrt-decomposition/sqrt-decomposition_1.cpp
@@ -1,18 +1,23 @@
-#include <stdio.h>
+#include <iostream>
 
 long long H(int n) {
   long long res = 0;  // 储存结果
   int l = 1, r;       // 块左端点与右端点
   while (l <= n) {
     r = n / (n / l);  // 计算当前块的右端点
-    res += (r - l + 1) * 1LL *
-           (n / l);  // 累加这一块的贡献到结果中。乘上 1LL 防止溢出
+    // 累加这一块的贡献到结果中。乘上 1LL 防止溢出
+    res += 1LL * (r - l + 1) * (n / l);
     l = r + 1;  // 左端点移到下一块
   }
   return res;
 }
 
-main() {
+int main() {
   int t, n;
-  for (scanf("%d", &t); t--; scanf("%d", &n), printf("%lld\n", H(n)));
+  std::cin >> t;
+  while (t--) {
+    std::cin >> n;
+    std::cout << H(n) << '\n';
+  }
+  return 0;
 }
