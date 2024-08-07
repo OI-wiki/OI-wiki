a9ed6f6d17d5349b5143f8716aa27c03de348b50
HikariLan <hikarilan@minecraft.kim>
2024-08-08 02:43:31 +0800

    feat: dark mode support

--- docs/_static/js/offsets-inject.js
+++ docs/_static/js/offsets-inject.js
@@ -1,5 +1,30 @@
+function matchColor() {
+  const palettle = localStorage.getItem("/.__palette");
+  if (
+    (palettle !== null && JSON.parse(palettle)?.color?.scheme !== "default") ||
+    (palettle === null && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("review_dark_mode");
+  } else {
+    document.documentElement.classList.remove("review_dark_mode");
+  }
+}
+
+function hookMkdocsMaterial() {
+  document.querySelector(".md-header__option").addEventListener("click", e => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+    setTimeout(matchColor, 0); // wait for the theme to be applied
+  });
+}
+
+hookMkdocsMaterial();
+
 document$.subscribe(function () {
-    OffsetsInjectionReview instanceof Object && OffsetsInjectionReview.setupReview instanceof Function && OffsetsInjectionReview.setupReview(document.body, {
-        apiEndpoint: "{apiEndpoint}", // api endpoint injected here, see: scripts/post-build/offsets-inject/task-handler.ts
-    })
-})
+  matchColor();
+
+  OffsetsInjectionReview instanceof Object &&
+    OffsetsInjectionReview.setupReview instanceof Function &&
+    OffsetsInjectionReview.setupReview(document.body, {
+      apiEndpoint: "{apiEndpoint}" // api endpoint injected here, see: scripts/post-build/offsets-inject/task-handler.ts
+    });
+});
