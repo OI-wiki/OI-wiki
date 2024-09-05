2d24818212d54be8b9149c575ea1078ebb1597e5
TrickEye <20373866@buaa.edu.cn>
2024-09-05 17:13:20 +0800

    refractor(sum-up.py): use arr(sysinfo, output)

--- scripts/sum-up.py
+++ scripts/sum-up.py
@@ -15,61 +15,19 @@ with open(os.environ.get('GITHUB_STEP_SUMMARY'), 'w') as f:
         print(f'\n## {key}\n')
         f.write(f'\n## **{key}**\n')
 
-        print(f'x86_64 Ubuntu 22.04')
-        f.write(f'\nx86_64 Ubuntu 22.04\n')
-        for line in OUTPUT_UBUNTU[key]:
-            print(f'- {line}: {OUTPUT_UBUNTU[key][line]}')
-            f.write(f'- {line}: {OUTPUT_UBUNTU[key][line]}\n')
-            if 'CE' in OUTPUT_UBUNTU[key][line]:
-                this_file_looks_odd = True
-            if 'RE' in OUTPUT_UBUNTU[key][line]:
-                this_file_looks_odd = True
-            if 'WA' in OUTPUT_UBUNTU[key][line]:
-                this_file_looks_odd = True
-        print(f'Arm64 macOS 12.0')
-        f.write(f'\nArm64 macOS 12.0\n')
-        for line in OUTPUT_MACOS[key]:
-            print(f'- {line}: {OUTPUT_MACOS[key][line]}')
-            f.write(f'- {line}: {OUTPUT_MACOS[key][line]}\n')
-            if 'CE' in OUTPUT_MACOS[key][line]:
-                this_file_looks_odd = True
-            if 'RE' in OUTPUT_MACOS[key][line]:
-                this_file_looks_odd = True
-            if 'WA' in OUTPUT_MACOS[key][line]:
-                this_file_looks_odd = True
-        print(f'x86_64 Alpine with MUSL')
-        f.write(f'\nx86_64 Alpine with MUSL\n')
-        for line in OUTPUT_ALPINE[key]:
-            print(f'- {line}: {OUTPUT_ALPINE[key][line]}')
-            f.write(f'- {line}: {OUTPUT_ALPINE[key][line]}\n')
-            if 'CE' in OUTPUT_ALPINE[key][line]:
-                this_file_looks_odd = True
-            if 'RE' in OUTPUT_ALPINE[key][line]:
-                this_file_looks_odd = True
-            if 'WA' in OUTPUT_ALPINE[key][line]:
-                this_file_looks_odd = True
-        print(f'x86_64 Windows 10')
-        f.write(f'\nx86_64 Windows 10\n')
-        for line in OUTPUT_WINDOWS[key]:
-            print(f'- {line}: {OUTPUT_WINDOWS[key][line]}')
-            f.write(f'- {line}: {OUTPUT_WINDOWS[key][line]}\n')
-            if 'CE' in OUTPUT_WINDOWS[key][line]:
-                this_file_looks_odd = True
-            if 'RE' in OUTPUT_WINDOWS[key][line]:
-                this_file_looks_odd = True
-            if 'WA' in OUTPUT_WINDOWS[key][line]:
-                this_file_looks_odd = True
-        print(f'RISC-V64 Ubuntu 22.04')
-        f.write(f'\nRISC-V64 Ubuntu 22.04\n')
-        for line in OUTPUT_RV[key]:
-            print(f'- {line}: {OUTPUT_RV[key][line]}')
-            f.write(f'- {line}: {OUTPUT_RV[key][line]}\n')
-            if 'CE' in OUTPUT_RV[key][line]:
-                this_file_looks_odd = True
-            if 'RE' in OUTPUT_RV[key][line]:
-                this_file_looks_odd = True
-            if 'WA' in OUTPUT_RV[key][line]:
-                this_file_looks_odd = True
+        for sysinfo, output in zip(['x86_64 Ubuntu 22.04', 'Arm64 macOS 12.0', 'x86_64 Alpine with MUSL', 'x86_64 Windows 10', 'RISC-V64 Ubuntu 22.04'], [OUTPUT_UBUNTU, OUTPUT_MACOS, OUTPUT_ALPINE, OUTPUT_WINDOWS, OUTPUT_RV]):
+            print(f'## {sysinfo}')
+            f.write(f'\n## {sysinfo}\n')
+            for line in output[key]:
+                print(f'- {line}: {output[key][line]}')
+                f.write(f'- {line}: {output[key][line]}\n')
+                if 'CE' in output[key][line]:
+                    this_file_looks_odd = True
+                if 'RE' in output[key][line]:
+                    this_file_looks_odd = True
+                if 'WA' in output[key][line]:
+                    this_file_looks_odd = True
+
         if this_file_looks_odd:
             any_file_looks_odd = True
             f.write(f'\n疑似存在未定义行为，请核实\n')
