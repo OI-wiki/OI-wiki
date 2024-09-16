39f310e23067cb5747d8d74fac26d41beb83bd6e
TrickEye <20373866@buaa.edu.cn>
2024-09-16 13:10:22 +0000

    fix: bug in my aux files
    I realized I haven't written any c++ files in the last 5 years

--- docs/basic/code/divide-and-conquer/divide-and-conquer_1.aux1.cpp
+++ docs/basic/code/divide-and-conquer/divide-and-conquer_1.aux1.cpp
@@ -6,30 +6,22 @@
 
 extern int pathSum(TreeNode *root, int sum);
 
-TreeNode *build_tree(char valstr[][20]) {
-  TreeNode *root = (strcmp(valstr[0], "null") == 0)
-                       ? nullptr
-                       : new TreeNode(atoi(valstr[0]));
-
-  return root;
-}
-
 int main() {
   // build tree
   int n;
   std::cin >> n;  // number of nodes
   TreeNode *treeNodes[MAXN];
   for (int i = 0; i < n; i++) {
-    char temp[20];
+    std::string temp;
     std::cin >> temp;
     treeNodes[i] =
-        (strcmp(temp, "null") == 0) ? nullptr : new TreeNode(atoi(temp));
+        (temp == "null") ? nullptr : new TreeNode(std::stoi(temp));
     if (i & 1)
       treeNodes[i >> 1]->left = treeNodes[i];
-    else
-      treeNodes[i >> 1]->right = treeNodes[i];
+    else if (i)
+      treeNodes[(i-1) >> 1]->right = treeNodes[i];
   }
   int sum;
   std::cin >> sum;
-  return pathSum(treeNodes[0], 0);
+  std::cout << pathSum(treeNodes[0], sum) << std::endl;
 }
