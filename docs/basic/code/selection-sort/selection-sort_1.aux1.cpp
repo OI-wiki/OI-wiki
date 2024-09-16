a85e292d145f1a2dc1503f077fa28f7fe00264fe
TrickEye <20373866@buaa.edu.cn>
2024-09-16 22:00:15 +0800

    fix error

--- docs/basic/code/selection-sort/selection-sort_1.aux1.cpp
+++ docs/basic/code/selection-sort/selection-sort_1.aux1.cpp
@@ -7,11 +7,11 @@ int main() {
   int a[MAXN];
   int n;
   std::cin >> n;
-  for (int i = 0; i < n; ++i) {
+  for (int i = 1; i <= n; ++i) {
     std::cin >> a[i];
   }
   selection_sort(a, n);
-  for (int i = 0; i < n; ++i) {
+  for (int i = 1; i <= n; ++i) {
     std::cout << a[i] << " ";
   }
   std::cout << std::endl;
