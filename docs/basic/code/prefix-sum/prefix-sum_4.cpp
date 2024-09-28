f04de7a90e160543bf65384f9e18b5ec8cd8b163
Claudy Forrest <41589268+c-forrest@users.noreply.github.com>
2024-09-28 02:56:48 -0500

    fix(prefix-sum): 增加 main 函数

--- docs/basic/code/prefix-sum/prefix-sum_4.cpp
+++ docs/basic/code/prefix-sum/prefix-sum_4.cpp
@@ -1,28 +1,30 @@
 #include <vector>
 
-int N1, N2, N3;
-std::vector<std::vector<std::vector<int>>> a(
-    N1 + 1, std::vector<std::vector<int>>(N2 + 1, std::vector<int>(N3 + 1)));
+int main() {
+    int N1, N2, N3;
+    std::vector<std::vector<std::vector<int>>> a(
+        N1 + 1, std::vector<std::vector<int>>(N2 + 1, std::vector<int>(N3 + 1)));
 
-std::vector<std::vector<std::vector<int>>> prefix_sum(
-    int N1, int N2, int N3, std::vector<std::vector<std::vector<int>>>& a) {
-  // Copy.
-  auto ps = a;
+    // Copy.
+    auto ps = a;
 
-  // Prefix-sum for 3rd dimension.
-  for (int i = 1; i <= N1; ++i)
-    for (int j = 1; j <= N2; ++j)
-      for (int k = 1; k <= N3; ++k) ps[i][j][k] += ps[i][j][k - 1];
+    // Prefix-sum for 3rd dimension.
+    for (int i = 1; i <= N1; ++i)
+        for (int j = 1; j <= N2; ++j)
+            for (int k = 1; k <= N3; ++k) 
+                ps[i][j][k] += ps[i][j][k - 1];
 
-  // Prefix-sum for 2nd dimension.
-  for (int i = 1; i <= N1; ++i)
-    for (int j = 1; j <= N2; ++j)
-      for (int k = 1; k <= N3; ++k) ps[i][j][k] += ps[i][j - 1][k];
+    // Prefix-sum for 2nd dimension.
+    for (int i = 1; i <= N1; ++i)
+        for (int j = 1; j <= N2; ++j)
+            for (int k = 1; k <= N3; ++k) 
+                ps[i][j][k] += ps[i][j - 1][k];
 
-  // Prefix-sum for 1st dimension.
-  for (int i = 1; i <= N1; ++i)
-    for (int j = 1; j <= N2; ++j)
-      for (int k = 1; k <= N3; ++k) ps[i][j][k] += ps[i - 1][j][k];
+    // Prefix-sum for 1st dimension.
+    for (int i = 1; i <= N1; ++i)
+        for (int j = 1; j <= N2; ++j)
+            for (int k = 1; k <= N3; ++k) 
+                ps[i][j][k] += ps[i - 1][j][k];
 
-  return ps;
+    return 0;
 }
