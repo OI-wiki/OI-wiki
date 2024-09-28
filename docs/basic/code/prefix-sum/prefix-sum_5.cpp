b8172cad654446c152e546b99238b2c470adb143
Claudy Forrest <41589268+c-forrest@users.noreply.github.com>
2024-09-28 02:47:12 -0500

    fix(prefix-sum): 添加函数体

--- docs/basic/code/prefix-sum/prefix-sum_5.cpp
+++ docs/basic/code/prefix-sum/prefix-sum_5.cpp
@@ -3,17 +3,19 @@
 int n;
 std::vector<int> a(1 << n);
 
-// Copy.
-auto ps = a;
-
-// Loop over dimensions.
-for (int i = 0; i < n; ++i) {
-  // Loop over i-th dimension.
-  for (int st = 0; st < (1 << n); ++st) {
-    // This condition implies that i-th dimension is 1.
-    if ((st >> i) & 1) {
-      // ps[... 1 ...] += ps[... 0 ...]. (i-th dimension)
-      ps[st] += ps[st ^ (1 << i)];
+std::vector<int> sos(int n, std::vector<int>& a) {
+    // Copy.
+    auto ps = a;
+    // Loop over dimensions.
+    for (int i = 0; i < n; ++i) {
+        // Loop over i-th dimension.
+        for (int st = 0; st < (1 << n); ++st) {
+            // This condition implies that i-th dimension is 1.
+            if ((st >> i) & 1) {
+                // ps[... 1 ...] += ps[... 0 ...]. (i-th dimension)
+                ps[st] += ps[st ^ (1 << i)];
+            }
+        }
     }
-  }
+    return ps;
 }
