7b485586070fb1ddafafd0a7c933578e2e2337d9
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2024-09-30 09:29:42 +0000

    refactor(convex-hull_1.cpp): better impl

--- docs/geometry/code/convex-hull/convex-hull_1.cpp
+++ docs/geometry/code/convex-hull/convex-hull_1.cpp
@@ -1,106 +1,78 @@
-#include <algorithm>
-#include <cmath>
-#include <cstring>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 5;
-int n, m, T;
+using i64 = int64_t;
+using isz = ptrdiff_t;
+using usz = size_t;
 
+template <class T>
 struct Point {
-  double x, y;
+  T x, y;
+  Point(T x = 0, T y = 0) : x(x), y(y) {}
 
-  Point(double x = 0, double y = 0) : x(x), y(y) {}
+  friend Point operator+(const Point &a, const Point &b) { return {a.x + b.x, a.y + b.y}; }
+  friend Point operator-(const Point &a, const Point &b) { return {a.x - b.x, a.y - b.y}; }
+  // 点乘
+  friend T operator*(const Point &a, const Point &b) { return a.x * b.x + a.y * b.y; }
+  // 叉乘
+  friend T operator^(const Point &a, const Point &b) { return a.x * b.y - a.y * b.x; }
+};
 
-  Point operator+(const Point &p) const { return Point(x + p.x, y + p.y); }
-
-  Point operator-(const Point &p) const { return Point(x - p.x, y - p.y); }
-
-  double operator*(const Point &p) const { return x * p.y - y * p.x; }
-
-  bool operator<(const Point p) const {
-    return (x == p.x) ? (y < p.y) : (x < p.x);
+template <class T>
+vector<Point<T>> convex_hull(vector<Point<T>> p) {
+  assert(!p.empty());
+  sort(p.begin(), p.end(), [](const auto &a, const auto &b) { return a.x < b.x; });
+  vector<Point<T>> u{p[0]}, d{p.back()};
+  for (usz i = 1; i < p.size(); ++i) {
+    while (u.size() >= 2 && ((u.back() - u[u.size() - 2]) ^ (p[i] - u.back())) > 0) u.pop_back();
+    u.push_back(p[i]);
   }
-} a[N], b[N], zo, tmp[N];
-
-int sgn(double x) { return (fabs(x) == 0) ? 0 : ((x > 0) ? 1 : -1); }
-
-struct Convex {
-  Point p[N];
-  int cnt;
-
-  void init() {
-    cnt = 0;
-    std::fill(p, p + N, Point{});
+  for (usz i = p.size() - 2; (isz)i >= 0; --i) {
+    while (d.size() >= 2 && ((d.back() - d[d.size() - 2]) ^ (p[i] - d.back())) > 0) d.pop_back();
+    d.push_back(p[i]);
   }
-
-  void Andrew(int n, Point *v) {
-    sort(v + 1, v + n + 1), cnt = 0;
-    for (int i = 1; i <= n; i++) {
-      while (cnt > 1 && ((p[cnt] - p[cnt - 1]) * (v[i] - p[cnt])) <= 0) cnt--;
-      p[++cnt] = v[i];
-    }
-    int basic = cnt;
-    for (int i = n - 1; i >= 1; i--) {
-      while (cnt > basic && sgn(((p[cnt] - p[cnt - 1]) * (v[i] - p[cnt]))) <= 0)
-        cnt--;
-      p[++cnt] = v[i];
-    }
-    if (n > 1) cnt--;
-  }
-
-  void minkowski(Convex &a, Convex &b) {
-    static Convex s1, s2;
-    s1.init(), s2.init();
-    for (int i = 1; i < a.cnt; i++) s1.p[i] = a.p[i + 1] - a.p[i];
-    for (int i = 1; i < b.cnt; i++) s2.p[i] = b.p[i + 1] - b.p[i];
-    s2.p[b.cnt] = b.p[1] - b.p[b.cnt], s1.p[a.cnt] = a.p[1] - a.p[a.cnt];
-    int i = 1, j = 1;
-    p[++cnt] = a.p[1] + b.p[1];
-    while (i <= a.cnt && j <= b.cnt) {
-      if (s1.p[i] * s2.p[j] > 0)
-        p[cnt + 1] = p[cnt] + s1.p[i++];
-      else
-        p[cnt + 1] = p[cnt] + s2.p[j++];
-      cnt++;
-    }
-    while (i <= a.cnt) p[cnt + 1] = p[cnt] + s1.p[i++], cnt++;
-    while (j <= b.cnt) p[cnt + 1] = p[cnt] + s2.p[j++], cnt++;
-  }
-} a1, a2, s;
-
-double sqr(double a) { return a * a; }
-
-double calc(Point a, Point b) { return sqrt(sqr(a.x - b.x) + sqr(a.y - b.y)); }
-
-bool cmp(Point a, Point b) {
-  double x = a * b;
-  return (x == 0) ? (calc(a, zo) < calc(b, zo)) : (x > 0);
+  u.insert(u.end(), d.begin() + 1, d.end());
+  return u;
 }
 
-bool query(Point x) {
-  if (x * s.p[2] > 0 || s.p[s.cnt] * x > 0 ||
-      (x * s.p[2] == 0 && calc(x, zo) > calc(s.p[2], zo)) ||
-      (x * s.p[s.cnt] == 0 && calc(x, zo) > calc(s.p[s.cnt], zo)))
-    return 1;
-  int w = lower_bound(s.p + 2, s.p + s.cnt + 1, x, cmp) - s.p - 1;
-  return (x - s.p[w]) * (s.p[w + 1] - s.p[w]) > 0;
+template <class T>
+vector<Point<T>> minkowski_sum(vector<Point<T>> a, vector<Point<T>> b) {
+  vector<Point<T>> c{a[0] + b[0]};
+  for (usz i = 0; i + 1 < a.size(); ++i) a[i] = a[i + 1] - a[i];
+  for (usz i = 0; i + 1 < b.size(); ++i) b[i] = b[i + 1] - b[i];
+  a.pop_back(), b.pop_back();
+  c.resize(a.size() + b.size() + 1);
+  merge(a.begin(), a.end(), b.begin(), b.end(), c.begin() + 1, [](const auto &a, const auto &b) { return (a ^ b) < 0; });
+  for (usz i = 1; i < c.size(); ++i) c[i] = c[i] + c[i - 1];
+  return c;
 }
 
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
-  cin >> n >> m >> T;
-  for (int i = 1; i <= n; i++) cin >> a[i].x >> a[i].y;
-  for (int i = 1; i <= m; i++) (cin >> b[i].x >> b[i].y), b[i] = zo - b[i];
-  a1.Andrew(n, a), a2.Andrew(m, b);
-  s.minkowski(a1, a2);
-  for (int i = 1; i <= s.cnt; i++) tmp[i] = s.p[i];
-  s.Andrew(s.cnt, tmp);
-  Point k = s.p[1];
-  for (int i = 1; i <= s.cnt; i++) s.p[i] = s.p[i] - k;
-  Point q;
-  while (T--) {
-    cin >> q.x >> q.y;
-    cout << !query(q - k) << '\n';
+  uint32_t n, m, q;
+  vector<Point<i64>> a, b;
+  cin >> n >> m >> q;
+  a.resize(n), b.resize(m);
+  for (auto &[x, y] : a) cin >> x >> y;
+  for (auto &[x, y] : b) cin >> x >> y, x = -x, y = -y;
+  a = convex_hull(a), b = convex_hull(b);
+  a = minkowski_sum(a, b);
+  a.pop_back();
+  for (usz i = 1; i < a.size(); ++i) a[i] = a[i] - a[0];
+  while (q--) {
+    Point<i64> v;
+    cin >> v.x >> v.y;
+    v = v - a[0];
+    if (v.x < 0) {
+      cout << "0\n";
+      continue;
+    }
+    auto it = upper_bound(a.begin() + 1, a.end(), v, [](const auto &a, const auto &b) { return (a ^ b) < 0; });
+    if (it == a.begin() + 1 || it == a.end()) {
+      cout << "0\n";
+      continue;
+    }
+    i64 s0 = *it ^ *prev(it), s1 = v ^ *prev(it), s2 = *it ^ v;
+    cout << (s1 >= 0 && s2 >= 0 && s1 + s2 <= s0) << '\n';
   }
   return 0;
 }
