381604eddd1c3d97d6f4d0a81cdef1b9dcf5e248
TrickEye <20373866@buaa.edu.cn>
2024-10-07 17:00:46 +0800

    clean up for formal PR

--- docs/patch-remove-sum-up/code/code1.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-int main() {
-    int *ptr = NULL;
-    *ptr = 10; // Produce RE
-    std::cout << *ptr << std::endl;
-    return 0;
-}
--- docs/patch-remove-sum-up/code/code2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a + b << std::endl;
-}
