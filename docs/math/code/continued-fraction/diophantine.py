04a99813a4b09b02214da99c4e0cf52862f1b2fe
Claudy Forrest <41589268+c-forrest@users.noreply.github.com>
2024-12-02 03:21:06 -0600

    feat(continued-fraction): add example codes and test files

---
+++ docs/math/code/continued-fraction/diophantine.py
@@ -0,0 +1,34 @@
+# DIOPHANTINE EQUATION SOLVER.
+# Find the continued fraction representation of P/Q.
+def fraction(p, q):
+    a = []
+    while q:
+        a.append(p // q)
+        p, q = q, p % q
+    return a
+
+
+# Find the convergents of a continued fraction A.
+# Numerators and denominators stored separately in P and Q.
+def convergents(a):
+    p = [0, 1]
+    q = [1, 0]
+    for it in a:
+        p.append(p[-1] * it + p[-2])
+        q.append(q[-1] * it + q[-2])
+    return p, q
+
+
+# Return (x, y) such that Ax+By=C.
+# Assume that such (x, y) exists.
+def dio(A, B, C):
+    p, q = convergents(fraction(A, B))
+    C //= A // p[-1]  # divide by gcd(A, B)
+    t = (-1) if len(p) % 2 else 1
+    return t * C * q[-2], -t * C * p[-2]
+
+
+if __name__ == "__main__":
+    A, B, C = map(int, input().split())
+    x, y = dio(A, B, C)
+    print(A * x + B * y - C)  # Should be 0
