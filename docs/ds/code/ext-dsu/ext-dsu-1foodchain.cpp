64f01fef074e9007905fd5982b761e7a39548728
set <panjiawen2011@163.com>
2025-04-22 21:04:10 +0800

    feat(ds/ext-dsu.md): 增加了拓展域并查集的内容

---
+++ docs/ds/code/ext-dsu/ext-dsu-1foodchain.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 5e4;
+int n, m, fa[N * 3 + 5], sz[N * 3 + 5], ans;
+
+int query(int x){ return fa[x] == x ? x : (fa[x] = query(fa[x])); }
+
+void merge(int x, int y){
+	x = query(x);
+	y = query(y);
+	if(x == y) return;
+	if(sz[x] < sz[y]) swap(x, y);
+	sz[x] += sz[y];
+	fa[y] = x;
+}
+
+int main(){
+	int op, x, y;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	iota(fa + 1, fa + n * 3, 1);
+	fill(sz + 1, sz + n * 3, 1);
+	while(m --){
+		cin >> op >> x >> y;
+		if(x > n || y > n) ++ ans;
+		else if(op == 1){
+			if(query(x) == query(y + n) || query(x) == query(y + (n << 1))) ++ ans;
+			else{
+				merge(x, y);
+				merge(x + n, y + n);
+				merge(x + (n << 1), y + (n << 1));
+			}
+		}
+		else{
+			if(query(x) == query(y) || query(x) == query(y + n)) ++ ans;
+			else{
+				merge(x, y + (n << 1));
+				merge(x + n, y);
+				merge(x + (n << 1), y + n);
+			}
+		}
+	}
+	cout << ans;
+}
---
+++ docs/ds/code/ext-dsu/ext-dsu-trick.cpp
@@ -0,0 +1,2 @@
+const int N = xxx;
+int _a[N + 5 << 1], *a = _a + (N + 5);
