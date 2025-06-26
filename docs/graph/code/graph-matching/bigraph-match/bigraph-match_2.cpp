9cb9cc58bc38bcaf467ec8307d7855e638aa208b
Claudy Forrest <41589268+c-forrest@users.noreply.github.com>
2025-06-26 00:11:55 -0500

    refactor(bigraph-match.md): 整理二分图最大匹配页面 (#6281)
    refactor(bigraph-match.md): rewrite

--- docs/graph/code/graph-matching/bigraph-match/bigraph-match_2.cpp
+++ docs/graph/code/graph-matching/bigraph-match/bigraph-match_2.cpp
@@ -1,86 +1,97 @@
 #include <algorithm>
 #include <iostream>
 #include <queue>
+#include <tuple>
 #include <vector>
-using namespace std;
 
-int ct, n, t[100010], x, r[100010], ans, vis[100010], dis[100010];
-vector<int> to[100010];
-queue<int> q;
+struct BipartiteGraph {
+  int n1, n2;                       // number of vertices in X and Y, resp.
+  std::vector<std::vector<int>> g;  // edges from X to Y
+  std::vector<int> ma, mb;  // matches from X to Y and from Y to X, resp.
+  std::vector<int> dist;    // distance from unsaturated vertices in X.
 
-int DFS(int x) {
-  if (vis[x]) {
-    return 0;
-  }
-  vis[x] = 1;
-  for (auto i : to[x]) {
-    if (!r[i]) {
-      r[i] = x;
-      t[x] = i;
-      return 1;
-    }
-    if (dis[r[i]] == dis[x] + 1 && DFS(r[i])) {
-      r[i] = x;
-      t[x] = i;
-      return 1;
-    }
-  }
-  return 0;
-}
+  BipartiteGraph(int n1, int n2)
+      : n1(n1), n2(n2), g(n1), ma(n1, -1), mb(n2, -1) {}
 
-int BFS() {
-  fill(vis + 1, vis + n + 1, 0);
-  fill(dis + 1, dis + n + 1, 0);
-  for (int i = 1; i <= n; i++) {
-    if (!t[i]) {
-      q.push(i);
-      dis[i] = 1;
-    }
-  }
-  int f = 0;
-  for (; q.size(); q.pop()) {
-    int tmp = q.front();
-    for (auto i : to[tmp]) {
-      if (!r[i]) {
-        f = 1;
+  // Add an edge from u in X to v in Y.
+  void add_edge(int u, int v) { g[u].emplace_back(v); }
+
+  // Build the level graph.
+  bool bfs() {
+    dist.assign(n1, -1);
+    std::queue<int> q;
+    for (int u = 0; u < n1; ++u) {
+      if (ma[u] == -1) {
+        dist[u] = 0;
+        q.emplace(u);
       }
-      if (r[i]) {
-        if (!dis[r[i]]) {
-          dis[r[i]] = dis[tmp] + 1;
-          q.push(r[i]);
+    }
+    // Build the level graph for all reachable vertices.
+    bool succ = false;
+    while (!q.empty()) {
+      int u = q.front();
+      q.pop();
+      for (int v : g[u]) {
+        if (mb[v] == -1) {
+          succ = true;
+        } else if (dist[mb[v]] == -1) {
+          dist[mb[v]] = dist[u] + 1;
+          q.emplace(mb[v]);
         }
       }
     }
+    return succ;
   }
-  return f;
-}
+
+  // Find an augmenting path starting at u.
+  bool dfs(int u) {
+    for (int v : g[u]) {
+      if (mb[v] == -1 || (dist[mb[v]] == dist[u] + 1 && dfs(mb[v]))) {
+        ma[u] = v;
+        mb[v] = u;
+        return true;
+      }
+    }
+    dist[u] = -1;  // Mark this point as inreachable after one visit.
+    return false;
+  }
+
+  // Hopcroft-Karp maximum matching algorithm.
+  std::vector<std::pair<int, int>> hopcroft_karp_maximum_matching() {
+    // Build the level graph and then find a blocking flow.
+    while (bfs()) {
+      for (int u = 0; u < n1; ++u) {
+        if (ma[u] == -1) {
+          dfs(u);
+        }
+      }
+    }
+    // Collect the matched pairs.
+    std::vector<std::pair<int, int>> matches;
+    matches.reserve(n1);
+    for (int u = 0; u < n1; ++u) {
+      if (ma[u] != -1) {
+        matches.emplace_back(u, ma[u]);
+      }
+    }
+    return matches;
+  }
+};
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr), cout.tie(nullptr);
-  for (cin >> ct; ct--;) {
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        cin >> x;
-        if (x) {
-          to[i].push_back(j);
-        }
-      }
-    }
-    for (; BFS();) {
-      for (int i = 1; i <= n; i++) {
-        if (!t[i] && DFS(i)) {
-          ans++;
-        }
-      }
-    }
-    cout << (ans == n ? "Yes" : "No") << '\n';
-    for (int i = 1; i <= n; i++) {
-      t[i] = r[i] = 0;
-      to[i].clear();
-    }
-    ans = 0;
+  std::ios::sync_with_stdio(false), std::cin.tie(nullptr);
+  int n1, n2, m;
+  std::cin >> n1 >> n2 >> m;
+  BipartiteGraph gr(n1, n2);
+  for (int i = 0; i < m; ++i) {
+    int u, v;
+    std::cin >> u >> v;
+    gr.add_edge(u, v);
+  }
+  auto res = gr.hopcroft_karp_maximum_matching();
+  std::cout << res.size() << '\n';
+  for (int i = 0; i < res.size(); ++i) {
+    std::cout << res[i].first << ' ' << res[i].second << '\n';
   }
   return 0;
 }
