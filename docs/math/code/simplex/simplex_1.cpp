19dfef85f5588714af842c2bbe87eb10b869cc42
ZnPdCo <66149312+ZnPdCo@users.noreply.github.com>
2025-07-09 11:07:13 +0800

    feat(*): add code and image

--- docs/math/code/simplex/simplex_1.cpp
+++ docs/math/code/simplex/simplex_1.cpp
@@ -1,61 +1,109 @@
-#include <cmath>
-#include <cstring>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-constexpr int M = 10005, N = 1005, INF = 1e9;
-
-int n, m;
-double a[M][N], b[M], c[N], v;
-
-void pivot(int l, int e) {  // 转轴操作函数
-  b[l] /= a[l][e];
-  for (int j = 1; j <= n; j++)
-    if (j != e) a[l][j] /= a[l][e];
-  a[l][e] = 1 / a[l][e];
-
-  for (int i = 1; i <= m; i++)
-    if (i != l && fabs(a[i][e]) > 0) {
-      b[i] -= a[i][e] * b[l];
-      for (int j = 1; j <= n; j++)
-        if (j != e) a[i][j] -= a[i][e] * a[l][j];
-      a[i][e] = -a[i][e] * a[l][e];
-    }
-
-  v += c[e] * b[l];
-  for (int j = 1; j <= n; j++)
-    if (j != e) c[j] -= c[e] * a[l][j];
-  c[e] = -c[e] * a[l][e];
-
-  // swap(B[l],N[e])
+#define N 100
+#define ll long long
+#define ld long double
+#define eps 1e-8
+#define inf 1e15
+ll n, m, t;
+ld a[N][N];
+ll r[N];
+ld ans[N];
+std::mt19937 rnd(time(0));
+void pivot(ll l, ll e) {
+	swap(r[n + l], r[e]);
+	ld t = a[l][e];
+	a[l][e] = 1;
+	for(ll i = 0; i <= n; i ++) a[l][i] /= t;
+	for(ll i = 0; i <= m; i ++) if(i != l && abs(a[i][e]) > eps) {
+		t = a[i][e]; a[i][e] = 0;
+		for(ll j = 0; j <= n; j ++) {
+			a[i][j] -= a[l][j] * t;
+		}
+	}
 }
-
-double simplex() {
-  while (true) {
-    int e = 0, l = 0;
-    for (e = 1; e <= n; e++)
-      if (c[e] > (double)0) break;
-    if (e == n + 1) return v;  // 此时v即为最优解
-    double mn = INF;
-    for (int i = 1; i <= m; i++) {
-      if (a[i][e] > (double)0 && mn > b[i] / a[i][e]) {
-        mn = b[i] / a[i][e];  // 找对这个e限制最紧的l
-        l = i;
-      }
-    }
-    if (mn == INF) return INF;  // unbounded
-    pivot(l, e);                // 转动l,e
-  }
+void noise() {
+	for(ll i = 1; i <= 10; i ++) {
+		ll l = 0, e = 0;
+		for(ll i = 1; i <= m; i ++) {
+			if(abs(a[i][0]) > eps && (!l || rnd() % 2 == 0)) {
+				l = i;
+			}
+		}
+		if(!l) break;
+		for(ll i = 1; i <= n; i ++) {
+			if(abs(a[l][i]) > eps && (!e || rnd() % 2 == 0)) {
+				e = i;
+			}
+		}
+		if(!e) continue;
+		pivot(l, e);
+	}
 }
-
+void init() {
+	while(1) {
+		ll l = 0, e = 0;
+		for(ll i = 1; i <= m; i ++) {
+			if(a[i][0] < -eps && (!l || rnd() % 2 == 0)) {
+				l = i;
+			}
+		}
+		if(!l) break;
+		for(ll i = 1; i <= n; i ++) {
+			if(a[l][i] < -eps && (!e || rnd() % 2 == 0)) {
+				e = i;
+			}
+		}
+		if(!e) {
+			printf("Infeasible");
+			exit(0);
+		}
+		pivot(l, e);
+	}
+}
+void simplex() {
+	while(1) {
+		ll l = 0, e = 0;
+		ld mn = inf;
+		for(ll i = 1; i <= n; i ++) {
+			if(a[0][i] > eps) {
+				e = i;
+				break;
+			}
+		}
+		if(!e) break;
+		for(ll i = 1; i <= m; i ++) {
+			if(a[i][e] > eps && a[i][0] / a[i][e] < mn) {
+				l = i;
+				mn = a[i][0] / a[i][e];
+			}
+		}
+		if(!l) {
+			printf("Unbounded");
+			exit(0);
+		}
+		pivot(l, e);
+	}
+}
 int main() {
-  cin.tie(nullptr)->sync_with_stdio(false);
-  cin >> n >> m;
-  for (int i = 1; i <= n; i++) cin >> c[i];
-  for (int i = 1; i <= m; i++) {
-    int s, t;
-    cin >> s >> t;
-    for (int j = s; j <= t; j++) a[i][j] = 1;  // 表示第i种志愿者在j时间可以服务
-    cin >> b[i];
-  }
-  cout << (int)(simplex() + 0.5);
+	scanf("%lld %lld %lld", &n, &m, &t);
+	for(ll i = 1; i <= n; i ++) {
+		scanf("%Lf", &a[0][i]);
+	}
+	for(ll i = 1; i <= m; i ++) {
+		for(ll j = 1; j <= n; j ++) {
+			scanf("%Lf", &a[i][j]);
+		}
+		scanf("%Lf", &a[i][0]);
+	}
+	for(ll i = 1; i <= n; i ++) r[i] = i;
+	noise();
+	init();
+	simplex();
+	if(abs(a[0][0]) < eps) printf("0\n");
+	else printf("%.10Lf\n", -a[0][0]);
+	if(t) {
+		for(ll i = 1; i <= m; i ++) ans[r[n + i]] = a[i][0];
+		for(ll i = 1; i <= n; i ++) printf("%.10Lf ", ans[i]);
+	}
 }
