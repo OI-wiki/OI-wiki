83f0bfbef64b413cb3246eb982cae56c72452d94
Hope <2310639991@qq.com>
2025-07-19 13:27:56 +0800

    fix(dp/code/dp-of-dp/dp-of-dp_2.cpp): fix CE probs of dp-of-dp_2.cpp

--- docs/dp/code/dp-of-dp/dp-of-dp_2.cpp
+++ docs/dp/code/dp-of-dp/dp-of-dp_2.cpp
@@ -1,29 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
 #define umap unordered_map
 #define pr pair
-#define st first
-#define nd second
+#define fst first
+#define snd second
 #define pc putchar
 #define pb push_back
-#define rep(i, x, y) for (int i = x; i <= y; i++)
-#define per(i, x, y) for (int i = y; i >= x; i--)
+#define REP(i, x, y) for (int i = x; i <= y; i++)
+#define PER(i, x, y) for (int i = y; i >= x; i--)
 #define ws(x) (write(x), pc(' '))
 #define we(x) (write(x), pc('\n'))
-using namespace std;
-const int N = 100, M = 400, mod = 998244353 /*1e9+7*/;
 
-inline void read(register int& a) {
+const int N = 100, M = 400, mod = 998244353;
+
+inline void read(int& a) {
   a = 0;
-  register char c;
-  register int f = 1;
-  while ((c = getchar()) < 48)
-    if (c == 45) f = -1;
+  char c;
+  int f = 1;
+  while ((c = getchar()) < '0')
+    if (c == '-') f = -1;
   do a = (a << 3) + (a << 1) + (c ^ 48);
-  while ((c = getchar()) > 47);
+  while ((c = getchar()) >= '0');
   a *= f;
 }
 
-inline void write(register int x) {
+inline void write(int x) {
   if (x < 0) putchar('-'), x = -x;
   if (x > 9) write(x / 10);
   putchar(x % 10 + '0');
@@ -32,7 +38,7 @@ inline void write(register int x) {
 int n, m, a[N + 5], fact[M + 5], inv[M + 5];
 
 inline int C(int x, int y) {
-  return 1ll * fact[x] * inv[y] % mod * inv[x - y] % mod;
+  return 1LL * fact[x] * inv[y] % mod * inv[x - y] % mod;
 }
 
 class HuAutomation {
@@ -42,31 +48,31 @@ class HuAutomation {
     int f[3][3];
 
    public:
-    Mat() { rep(i, 0, 2) rep(j, 0, 2) f[i][j] = -1; }
+    Mat() { REP(i, 0, 2) REP(j, 0, 2) f[i][j] = -1; }
 
     int* operator[](const int& x) { return f[x]; }
 
     inline bool operator==(Mat x) const {
-      rep(i, 0, 2) rep(j, 0, 2) if (f[i][j] != x[i][j]) return 0;
+      REP(i, 0, 2) REP(j, 0, 2) if (f[i][j] != x[i][j]) return 0;
       return 1;
     }
 
     inline bool operator<(Mat x) const {
-      rep(i, 0, 2)
-              rep(j, 0, 2) if (f[i][j] != x[i][j]) return f[i][j] < x[i][j];
+      REP(i, 0, 2)
+        REP(j, 0, 2) if (f[i][j] != x[i][j]) return f[i][j] < x[i][j];
       return 0;
     }
 
     inline bool Check() {
-      rep(i, 0, 2) rep(j, 0, 2) if (f[i][j] > 3) return 1;
+      REP(i, 0, 2) REP(j, 0, 2) if (f[i][j] > 3) return 1;
       return 0;
     }
 
     inline void Upd(Mat x, int t) {
-      rep(i, 0, 2)
-          rep(j, 0, 2) if (x[i][j] != -1) for (int k = 0;
-                                               k < 3 && i + j + k <= t; k++)
-              f[j][k] = max(f[j][k], min(i + x[i][j] + (t - i - j - k) / 3, 4));
+      REP(i, 0, 2)
+        REP(j, 0, 2) if (x[i][j] != -1) 
+          for (int k = 0; k < 3 && i + j + k <= t; k++)
+            f[j][k] = max(f[j][k], min(i + x[i][j] + (t - i - j - k) / 3, 4));
     }
   };
 
@@ -75,14 +81,13 @@ class HuAutomation {
     Mat F[2];
 
     node() {
-      rep(i, 0, 4) state[i] = 0;
+      REP(i, 0, 4) state[i] = 0;
       t = 0;
-      rep(i, 0, 1) F[i] = Mat();
+      REP(i, 0, 1) F[i] = Mat();
     }
 
     inline bool Check() {
-      if (t == -1 || t >= 7 || F[1].Check()) return 1;
-      return 0;
+      return t == -1 || t >= 7 || F[1].Check();
     }
 
     node Hu() {
@@ -119,7 +124,7 @@ class HuAutomation {
     return tot;
   }
 
-  inline void Expend(int x) { rep(i, 0, 4) A[x].state[i] = Get(A[x] + i); }
+  inline void Expend(int x) { REP(i, 0, 4) A[x].state[i] = Get(A[x] + i); }
 
   inline node Hu() {
     node x;
@@ -146,9 +151,10 @@ class HuAutomation {
 
   void DP() {
     f[0][0][1] = 1;
-    rep(i, 1, n) per(j, 0, m) rep(k, 1, tot) if (f[i - 1][j][k])
-        rep(t, 0, 4 - a[i])(f[i][j + t][A[k].state[a[i] + t]] +=
-                            1ll * f[i - 1][j][k] * C(4 - a[i], t) % mod) %= mod;
+    REP(i, 1, n) PER(j, 0, m) REP(k, 1, tot) if (f[i - 1][j][k])
+      REP(t, 0, 4 - a[i])
+        (f[i][j + t][A[k].state[a[i] + t]] +=
+         1LL * f[i - 1][j][k] * C(4 - a[i], t) % mod) %= mod;
   }
 } Hfu;
 
@@ -164,16 +170,19 @@ inline long long qpow(long long x, int y) {
 
 void init(int n) {
   fact[0] = 1;
-  rep(i, 1, n) fact[i] = 1ll * fact[i - 1] * i % mod;
+  REP(i, 1, n) fact[i] = 1LL * fact[i - 1] * i % mod;
   inv[n] = qpow(fact[n], mod - 2);
-  per(i, 0, n - 1) inv[i] = 1ll * inv[i + 1] * (i + 1) % mod;
+  PER(i, 0, n - 1) inv[i] = 1LL * inv[i + 1] * (i + 1) % mod;
 }
 
-signed main() {
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
   int ans = 0;
   Hfu.Build();
   read(n);
-  rep(i, 1, 13) {
+  REP(i, 1, 13) {
     int x, y;
     read(x), read(y);
     ++a[x];
@@ -181,10 +190,11 @@ signed main() {
   m = (n << 2) - 13;
   init(m);
   Hfu.DP();
-  rep(i, 1, m) {
-    (ans += 1ll * Hfu.f[n][i][1] * fact[i] % mod * fact[m - i] % mod) %= mod;
-    rep(j, 3, Hfu.tot)(ans += 1ll * Hfu.f[n][i][j] * fact[i] % mod *
-                              fact[m - i] % mod) %= mod;
+  REP(i, 1, m) {
+    (ans += 1LL * Hfu.f[n][i][1] * fact[i] % mod * fact[m - i] % mod) %= mod;
+    REP(j, 3, Hfu.tot)
+      (ans += 1LL * Hfu.f[n][i][j] * fact[i] % mod * fact[m - i] % mod) %= mod;
   }
-  we(1ll * ans * inv[m] % mod + 1);
+  we(1LL * ans * inv[m] % mod + 1);
+  return 0;
 }
