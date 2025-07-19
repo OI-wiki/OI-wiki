40a00ff58575023dade722fb3b14c9126aa2332d
Hope <2310639991@qq.com>
2025-07-19 14:34:21 +0800

    style: format markdown files with remark-lint

--- docs/dp/code/dp-of-dp/dp-of-dp_2.cpp
+++ docs/dp/code/dp-of-dp/dp-of-dp_2.cpp
@@ -152,7 +152,7 @@ class HuAutomation {
     f[0][0][1] = 1;
     REP(i, 1, n)
     PER(j, 0, m)
-+     REP(k, 1, tot) if (f[i - 1][j][k])
+      REP(k, 1, tot) if (f[i - 1][j][k])
         REP(t, 0, 4 - a[i])(f[i][j + t][A[k].state[a[i] + t]] +=
                              1LL * f[i - 1][j][k] * C(4 - a[i], t) % mod) %= mod;
   }
