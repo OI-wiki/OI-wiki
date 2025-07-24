da3c131f9d6069e24ed8613aa95e3de229255766
ZnPdCo <66149312+ZnPdCo@users.noreply.github.com>
2025-07-24 22:47:00 +0800

    fix code

--- docs/misc/code/fsm/fsm_2.cpp
+++ docs/misc/code/fsm/fsm_2.cpp
@@ -1,41 +1,55 @@
-#include <cstring>
 #include <iostream>
 #include <map>
 #include <queue>
 #include <vector>
+#include <cstring>
 #define ll long long
+#define ull unsigned long long
 using namespace std;
-map<__int128, int> mp;
-int tot, cnt, T;
-int nxt[20000][10], type[20000], belong[20000], tag[20000], sz[20000],
-    trans[20000][10], ans[20000];
-vector<int> pre[20000][10], Q[20000], S[20000];
-
-int dfs(__int128 st) {
-  if (mp.count(st)) return mp[st];
-  int res = mp[st] = ++tot;
-  for (int i = 0; i <= 90; i++)
-    if ((st >> i) & 1) {
-      type[res] = i;
-      break;
+struct node {
+    ull a1, a2;
+    node() { a1 = a2 = 0; }
+    int get(int x) {
+        if (x <= 63) return (a1 >> x) & 1;
+        return (a2 >> (x - 64)) & 1;
     }
-  for (int v = 0; v <= 9; v++) {
-    __int128 nw = 0;
-    for (int i = 0; i <= 90; i++)
-      if ((st >> i) & 1) {
-        if (i + v <= 90) nw |= (__int128(1) << (i + v));
-        if (i - v >= 0)
-          nw |= (__int128(1) << (i - v));
-        else
-          nw |= (__int128(1) << (v - i));
-      }
-    int t = dfs(nw);
-    nxt[res][v] = t;
-    pre[t][v].push_back(res);
-  }
-  return res;
+    void upd(int x, int v) {
+        if (x <= 63) {
+            if (v == 0) a1 &= ~(1ULL << x);
+            else a1 |= 1ULL << x;
+        } else {
+            if (v == 0) a2 &= ~(1ULL << (x - 64));
+            else a2 |= 1ULL << (x - 64);
+        }
+    }
+    friend bool operator<(const node &x, const node &y) {
+        return x.a1 < y.a1 || (x.a1 == y.a1 && x.a2 < y.a2);
+    }
+};
+map<node, int> mp;
+int tot, cnt, T;
+int nxt[20000][10], type[20000], belong[20000], tag[20000], sz[20000], trans[20000][10], ans[20000];
+vector<int> pre[20000][10], Q[20000], S[20000];
+int dfs(node &st) {
+    if (mp.count(st)) return mp[st];
+    int res = mp[st] = ++tot;
+    for (int i = 0; i <= 90; i++) if (st.get(i)) {
+        type[res] = i;
+        break;
+    }
+    for (int v = 0; v <= 9; v++) {
+        node nw;
+        for (int i = 0; i <= 90; i++) if (st.get(i)) {
+            if (i + v <= 90) nw.upd(i + v, 1);
+            if (i - v >= 0) nw.upd(i - v, 1);
+            else nw.upd(v - i, 1);
+        }
+        int t = dfs(nw);
+        nxt[res][v] = t;
+        pre[t][v].push_back(res);
+    }
+    return res;
 }
-
 void hopcroft() {
   const auto rebuild = [&](int u) {
     vector<int> tmp;
@@ -97,41 +111,39 @@ void hopcroft() {
   for (int i = 0; i < tot; i++)
     for (int c = 0; c <= 9; c++) trans[belong[i]][c] = belong[nxt[i][c]];
 }
-
 int a[20], len, f[20][718][10];
-
 ll F(int len, int lim, int x, int k) {
-  if (!lim && f[len][x][k] != -1) return f[len][x][k];
-  if (len == 0) return f[len][x][k] = (ans[x] <= k);
-  if (lim) {
+    if (!lim && f[len][x][k] != -1) return f[len][x][k];
+    if (len == 0) return f[len][x][k] = (ans[x] <= k);
+    if (lim) {
+        ll res = 0;
+        for (int i = 0; i < a[len]; i++) res += F(len - 1, 0, trans[x][i], k);
+        res += F(len - 1, 1, trans[x][a[len]], k);
+        return res;
+    }
     ll res = 0;
-    for (int i = 0; i < a[len]; i++) res += F(len - 1, 0, trans[x][i], k);
-    res += F(len - 1, 1, trans[x][a[len]], k);
-    return res;
-  }
-  ll res = 0;
-  for (int i = 0; i <= 9; i++) res += F(len - 1, 0, trans[x][i], k);
-  return f[len][x][k] = res;
+    for (int i = 0; i <= 9; i++) res += F(len - 1, 0, trans[x][i], k);
+    return f[len][x][k] = res;
 }
-
 ll calc(ll x, int k) {
-  len = 0;
-  while (x) {
-    a[++len] = x % 10;
-    x /= 10;
-  }
-  return F(len, 1, belong[1], k);
+    len = 0;
+    while (x) {
+        a[++len] = x % 10;
+        x /= 10;
+    }
+    return F(len, 1, belong[1], k);
 }
-
 int main() {
-  memset(f, -1, sizeof f);
-  dfs(1);
-  hopcroft();
-  scanf("%d", &T);
-  while (T--) {
-    ll l, r;
-    int k;
-    scanf("%lld%lld%d", &l, &r, &k);
-    printf("%lld\n", calc(r, k) - calc(l - 1, k));
-  }
+    memset(f, -1, sizeof f);
+    node st;
+    st.upd(0, 1);
+    dfs(st);
+    hopcroft();
+    scanf("%d", &T);
+    while (T--) {
+        ll l, r;
+        int k;
+        scanf("%lld%lld%d", &l, &r, &k);
+        printf("%lld\n", calc(r, k) - calc(l - 1, k));
+    }
 }
