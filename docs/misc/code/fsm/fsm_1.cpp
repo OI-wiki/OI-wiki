2f4d1f8b7eddd83a130703940af70ac8a6a496e8
Claudy Forrest <41589268+c-forrest@users.noreply.github.com>
2025-08-03 20:20:11 -0500

    fix: code

--- docs/misc/code/fsm/fsm_1.cpp
+++ docs/misc/code/fsm/fsm_1.cpp
@@ -1,200 +1,203 @@
-#include <algorithm>
-#include <cstring>
+#include "dfa.hpp"
+
+// --8<-- [start:main]
+#include <array>
 #include <iostream>
+#include <unordered_map>
 #include <vector>
-using namespace std;
-// 6 种需要用到的串所对应的 type 编号
-// 0:0
-// 1:1
-// 00:2
-// 01:3
-// 10:4
-// 11:5
-char op[9];
-int q, n;
-char a[100010];
 
-// l 表示每个字符串的后接字符串的接受状态
-// l.x, l.y 表示字符串的长度和内容
-// l.sta 表示字符串的后接字符串的接受状态
-struct node {
-  vector<bool> sta;
-  int x, y;
-} l[1 << 10];
+constexpr int L = 3;
+std::array<int, 1 << L> op;
+constexpr int X = (1 << L) - 2;
 
-int len = 0;
+// Build a DFA that accepts binary strings which evaluate to a given string.
+//   0b000 → "00"
+//   0b001 → "01"
+//   0b010 → "10"
+//   0b011 → "11"
+//   0b100 → "0"
+//   0b101 → "1"
+DFA build(int x) {
+  constexpr int L1 = 9, L2 = 6;
+  // The DP result for a string `st` of length `len` is stored at index (1 << len) | st.
+  const auto idx = [](int len, int st) -> int { return (1 << len) | st; };
 
-struct DFA {
-  // ac[i][s] 表示长度为 i 的串 s 是否可以接受
-  // acc[s] 表示最小化 dfa 后的状态 s 是否可以接受
-  // pm[s] 表示状态 s 的所对应等价类中长度最小的字符串的长度与内容
-  // mp[l][s] 表示长度为 l 的串 s 所对应的等价类编号
-  // f 是倍增数组，底层使用分块减小常数
-  // cnt 表示等价类个数
-  bool ac[16][1 << 15], acc[60];
-  pair<int, int> pm[60];
-  int mp[10][1 << 9];
-  unsigned char f[60][100010][12];
-  int cnt = 0;
+  // Compute which bit strings are accepted using dynamic programming.
+  std::vector<bool> acc(1 << (L1 + L2 + 1));
+  acc[(x >> 2) ? idx(1, x & 1) : idx(2, x)] = true;
 
-  inline void build(int x) {
-    if (x <= 1)
-      ac[1][x] = 1;
-    else
-      ac[2][x - 2] = 1;
-
-    // 跑出所有长度 <= 15 的串的接受状态
-    for (int i = 3; i <= 15; i++) {
-      for (int s = 0; s < (1 << i); s++) {
-        int s1 = 0, s2 = s;
-        for (int j = 0; j <= i - 3; j++) {
-          int t = ((((s2 >> 3) << 1) | op[s2 & 7]) << j) | s1;
-          ac[i][s] |= ac[i - 2][t];
-          s1 |= (s2 & 1) << j;
-          s2 >>= 1;
-        }
-      }
-    }
-
-    // 跑出所有长度 <= 9 的串的后接长度 <= 6 的串的接受状态
-    len = 0;
-    for (int i = 1; i <= 9; i++) {
-      for (int s = 0; s < (1 << i); s++) {
-        l[++len].sta.clear();
-        l[len].x = i, l[len].y = s, l[len].sta.push_back(ac[i][s]);
-        for (int j = 1; j <= 6; j++) {
-          for (int t = 0; t < (1 << j); t++) {
-            l[len].sta.push_back(ac[i + j][(s << j) | t]);
-          }
-        }
-      }
-    }
-
-    sort(l + 1, l + len + 1, [](node a, node b) {
-      if (a.sta != b.sta) return a.sta < b.sta;
-      return a.x < b.x;
-    });
-
-    for (int i = 1; i <= len; i++) {
-      if (i == 1 || l[i].sta != l[i - 1].sta) {
-        mp[l[i].x][l[i].y] = ++cnt;
-        pm[cnt] = make_pair(l[i].x, l[i].y);
-        acc[cnt] = ac[l[i].x][l[i].y];
-      } else {
-        mp[l[i].x][l[i].y] = cnt;
-      }
-    }
-  }
-
-  // 预处理倍增
-  inline void init() {
-    for (int i = 1; i <= cnt; i++) {
-      for (int j = 1; j <= (n >> 5); j++) {
-        int x = i;
-        for (int k = (j << 5); k < ((j + 1) << 5); k++) {
-          int l = pm[x].first, s = pm[x].second;
-          x = mp[l + 1][(s << 1) | a[k]];
-        }
-        f[i][j][0] = x;
-      }
-    }
-    for (int k = 1; k <= 11; k++) {
-      for (int j = 1; j + (1 << k) - 1 <= (n >> 5); j++) {
-        for (int i = 1; i <= cnt; i++) {
-          f[i][j][k] = f[f[i][j][k - 1]][j + (1 << (k - 1))][k - 1];
+  for (int len = (x >> 2) ? 3 : 4; len <= L1 + L2; len += 2) {
+    for (int st = 0; st < (1 << len); ++st) {
+      for (int i = 0; i + L <= len; ++i) {
+        // Replace st[i..i+2] using the op[] rule, and check if the result is accepted.
+        // Result = st[0..i-1] + op[st[i..i+2]] + st[i+3..end]
+        int pr = ((((st >> (i + L)) << 1) | op[(st >> i) & ((1 << L) - 1)]) << i) | (st & ((1 << i) - 1));
+        if (acc[idx(len - 2, pr)]) {
+          acc[idx(len, st)] = true;
+          break;
         }
       }
     }
   }
 
-  // 检查区间 [l, r] 是否可以接受
-  inline bool check(int l, int r) {
-    int x = mp[1][a[l]];
-    l++;
-    while (l <= r && (l & 31)) {
-      int i = pm[x].first, s = pm[x].second;
-      x = mp[i + 1][(s << 1) | a[l]];
-      l++;
+  // Construct a DFA using the Myhill-Nerode theorem.
+  DFA dfa(2);
+  std::unordered_map<std::vector<bool>, int> mp;  // Maps characteristic vectors to state IDs.
+  std::vector<int> sts;                           // Representative 01 strings for each state.
+  std::vector<int> ids(1 << (L1 + 1));            // Maps 01 strings to DFA state IDs.
+
+  for (int len = 0; len <= L1; ++len) {
+    for (int st = (1 << len); st < (2 << len); ++st) {
+      // Construct characteristic vector for this prefix.
+      std::vector<bool> key(1 << (L2 + 1));
+      for (int nxt = 0; nxt <= L2; ++nxt)
+        std::copy(acc.begin() + (st << nxt), acc.begin() + ((st + 1) << nxt), key.begin() + (1 << nxt));
+      if (!mp.count(key)) {
+        mp[key] = dfa.n++;
+        sts.push_back(st);
+        dfa.acc.push_back(key[idx(0, 0)]);
+      }
+      ids[st] = mp[key];
     }
-    for (int k = 11; k >= 0; k--) {
-      if (l + (1 << (k + 5)) - 1 <= r) {
-        x = f[x][l >> 5][k];
-        l += (1 << (k + 5));
+  }
+
+  // Build transitions.
+  for (int c = 0; c < 2; ++c) {
+    dfa.trans[c].resize(dfa.n);
+    for (int i = 0; i < dfa.n; ++i)
+      dfa.trans[c][i] = ids[(sts[i] << 1) | c];
+  }
+
+  return dfa;
+}
+
+// Initialize the DFA's.
+std::vector<DFA> dfa;
+void init() {
+  std::string s;
+  std::cin >> s;
+  for (int i = 0; i < 8; ++i) op[i] = s[i] - '0';
+  std::swap(op[1], op[4]);
+  std::swap(op[3], op[6]);
+  for (int x = 0; x < X; ++x) dfa.push_back(build(x));
+}
+
+std::string s;
+constexpr int B = 5; // Block size is 2^B.
+std::vector<std::vector<std::array<std::vector<int>, X>>> pre;
+
+void precompute() {
+  int n = s.size();
+  for (int i = 0; i < n; ++i) s[i] -= '0';
+  int nn = ((n - 1) >> B) + 1;
+  pre.clear();
+
+  // Compute transitions over single blocks (2^B characters) for each DFA.
+  pre.emplace_back(nn);
+  for (int i = 0; i < nn; ++i) {
+    for (int x = 0; x < X; ++x) {
+      pre[0][i][x].resize(dfa[x].n);
+      for (int j = 0; j < dfa[x].n; ++j) {
+        int cr = j;
+        for (int ii = (i << B); ii < ((i + 1) << B) && ii < n; ++ii)
+          cr = dfa[x].trans[s[ii]][cr];
+        pre[0][i][x][j] = cr;
       }
     }
-    while (l <= r) {
-      int i = pm[x].first, s = pm[x].second;
-      x = mp[i + 1][(s << 1) | a[l]];
-      l++;
-    }
-    return acc[x];
   }
-} d[6];
 
-// 分治递归求解，solve(l, r, type) 表示区间 [l, r] 内的串的表示 type 的方案
-void solve(int l, int r, int type) {
-  if (l == r) {
-    putchar(a[l] + '0');
-    return;
+  // Build higher-level transitions using binary lifting.
+  for (int d = 1; (1 << d) <= nn; ++d) {
+    pre.emplace_back(nn - (1 << d) + 1);
+    for (int i = 0; i <= nn - (1 << d); ++i) {
+      for (int x = 0; x < X; ++x) {
+        pre[d][i][x].resize(dfa[x].n);
+        for (int j = 0; j < dfa[x].n; ++j)
+          pre[d][i][x][j] = pre[d - 1][i + (1 << (d - 1))][x][pre[d - 1][i][x][j]];
+      }
+    }
   }
-  if (type <= 1) {
-    // 启发式分裂，维护两个指针，一个从左往右扫，一个从右往左扫，以枚举断点
-    for (int i = l, j = r; i <= j; i += 2, j -= 2) {
-      for (int k = 7; k >= 0; k--) {
-        if (op[k] == type && d[k >> 2].check(l, i) &&
-            d[(k & 3) + 2].check(i + 1, r)) {
-          putchar('(');
-          solve(l, i, k >> 2);
-          solve(i + 1, r, (k & 3) + 2);
-          putchar(')');
+}
+
+// Check if the x-th DFA accepts s[l...r].
+bool check(int l, int r, int x) {
+  if (l > r) return false;
+  int cr = dfa[x].q0;
+  if ((l >> B) == (r >> B)) {
+    for (; l <= r; ++l) cr = dfa[x].trans[s[l]][cr];
+  } else {
+    for (; (l & ((1 << B) - 1)) && (l <= r); ++l)
+      cr = dfa[x].trans[s[l]][cr];
+    int d = (r >> B) - (l >> B);
+    for (int z = 0; z < (int)pre.size(); ++z)
+      if ((d >> z) & 1) {
+        cr = pre[z][l >> B][x][cr];
+        l += 1 << (z + B);
+      }
+    for (; l <= r; ++l) cr = dfa[x].trans[s[l]][cr];
+  }
+  return dfa[x].acc[cr];
+}
+
+// Construct expression for s[ll...rr].
+void calc(int ll, int rr, int x) {
+  if (ll == rr) return (void)(std::cout << (x & 1));
+  if (x >> 2) {
+    for (int l = ll, r = rr; l <= r; l += 2, r -= 2) {
+      for (int k = 0; k < 8; ++k) {
+        if (op[k] != (x & 1)) continue;
+        if (check(ll, l, (k >> 2) | 4) && check(l + 1, rr, k & 3)) {
+          std::cout << '(';
+          calc(ll, l, (k >> 2) | 4);
+          calc(l + 1, rr, k & 3);
+          std::cout << ')';
           return;
         }
-        if (op[k] == type && d[(k >> 1) + 2].check(l, j - 1) &&
-            d[k & 1].check(j, r)) {
-          putchar('(');
-          solve(l, j - 1, (k >> 1) + 2);
-          solve(j, r, k & 1);
-          putchar(')');
+        if (check(ll, r - 1, k >> 1) && check(r, rr, (k & 1) | 4)) {
+          std::cout << '(';
+          calc(ll, r - 1, k >> 1);
+          calc(r, rr, (k & 1) | 4);
+          std::cout << ')';
           return;
         }
       }
     }
   } else {
-    int k = type - 2;
-    for (int i = l, j = r; i <= j; i += 2, j -= 2) {
-      if (d[k >> 1].check(l, i) && d[k & 1].check(i + 1, r)) {
-        solve(l, i, k >> 1);
-        solve(i + 1, r, k & 1);
-        return;
+      for (int l = ll, r = rr; l <= r; l += 2, r -= 2) {
+        if (check(ll, l, (x >> 1) | 4) && (check(l + 1, rr, (x & 1) | 4))) {
+          calc(ll, l, (x >> 1) | 4);
+          calc(l + 1, rr, (x & 1) | 4);
+          return;
+        }
+        if (check(ll, r - 1, (x >> 1) | 4) && check(r, rr, (x & 1) | 4)) {
+          calc(ll, r - 1, (x >> 1) | 4);
+          calc(r, rr, (x & 1) | 4);
+          return;
+        }
       }
-      if (d[k >> 1].check(l, j - 1) && d[k & 1].check(j, r)) {
-        solve(l, j - 1, k >> 1);
-        solve(j, r, k & 1);
-        return;
-      }
-    }
   }
 }
 
+void solve() {
+  std::cin >> s;
+  int n = s.size();
+  precompute();
+  if (!check(0, n - 1, 0b101)) {
+    std::cout << -1 << '\n';
+    return;
+  }
+  calc(0, n - 1, 0b101);
+  std::cout << '\n';
+}
+
 int main() {
-  scanf("%s %d", op, &q);
-  swap(op[1], op[4]);
-  swap(op[3], op[6]);
-  for (int i = 0; i < 8; i++) op[i] -= '0';
-  for (int i = 0; i < 6; i++) d[i].build(i);
-  while (q--) {
-    scanf("%s", a + 1);
-    n = strlen(a + 1);
-    for (int i = 1; i <= n; i++) a[i] -= '0';
-    for (int i = 0; i < 6; i++) d[i].init();
-    if (!d[1].check(1, n)) {
-      putchar('-');
-      putchar('1');
-      putchar('\n');
-      continue;
-    }
-    solve(1, n, 1);
-    putchar('\n');
+  init();
+  int q;
+  std::cin >> q;    
+  for (; q; --q) {
+    solve();
   }
   return 0;
 }
+
+// --8<-- [end:main]
