289273190072f5cd33e5f2280670ee033c16ac3b
GoodFish <me@goodfish.site>
2025-08-23 22:59:15 +0800

    fix(graph/code/mst/): 修改原例题代码 源代码无法通过洛谷新增的hack数据
    源代码无法通过洛谷新增的hack数据，其没有判断 n==k 的情况 只有90pts。所以进行修改：
    
    源代码评测记录：https://www.luogu.com.cn/record/232849498
    
    新代码评测记录: https://www.luogu.com.cn/record/232850689

--- docs/graph/code/mst/mst_3.cpp
+++ docs/graph/code/mst/mst_3.cpp
@@ -1,65 +1,55 @@
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
+
 using namespace std;
+typedef long long ll;
 
-int fa[1010];  // 定义父亲
-int n, m, k;
+ll fat[1005]; // 定义父亲
+ll n, m, k, ecnt = 0;
 
-struct edge {
-  int u, v, w;
-};
+struct Edge { // 结构体存边
+	ll u, v, w;
+} edge[10005];
 
-int l;
-edge g[10010];
-
-void add(int u, int v, int w) {
-  l++;
-  g[l].u = u;
-  g[l].v = v;
-  g[l].w = w;
+bool cmp(Edge a, Edge b) { // sort排序结构体用
+	return a.w < b.w;
 }
 
-// 标准并查集
-int findroot(int x) { return fa[x] == x ? x : fa[x] = findroot(fa[x]); }
-
-void Merge(int x, int y) {
-  x = findroot(x);
-  y = findroot(y);
-  fa[x] = y;
+void init(ll n) { // 初始化并查集
+	for (int i = 0; i < n; ++i) fat[i] = i;
 }
 
-bool cmp(edge A, edge B) { return A.w < B.w; }
-
-// Kruskal 算法
-void kruskal() {
-  int tot = 0;  // 存已选了的边数
-  int ans = 0;  // 存总的代价
-  for (int i = 1; i <= m; i++) {
-    int xr = findroot(g[i].u), yr = findroot(g[i].v);
-    if (xr != yr) {   // 如果父亲不一样
-      Merge(xr, yr);  // 合并
-      tot++;          // 边数增加
-      ans += g[i].w;  // 代价增加
-    }
-    if (tot >= (n - k)) {  // 检查选的边数是否满足 k 个棉花糖
-      cout << ans << '\n';
-      return;
-    }
-  }
-  cout << "No Answer\n";  // 无法连成
+ll find(int a) { // 并查集查询
+	if (fat[a] != a) fat[a] = find(fat[a]);
+	return fat[a];
 }
 
-int main() {
-  cin >> n >> m >> k;
-  for (int i = 1; i <= n; i++) {  // 初始化
-    fa[i] = i;
-  }
-  for (int i = 1; i <= m; i++) {
-    int u, v, w;
-    cin >> u >> v >> w;
-    add(u, v, w);  // 添加边
-  }
-  sort(g + 1, g + m + 1, cmp);  // 先按边权排序
-  kruskal();
-  return 0;
+void merge(ll a, ll b) { // 并查集合并
+	fat[find(a)] = find(b);
+}
+
+ll kruskal() { // kruskal最小生成树算法
+	ll cost = 0, tcnt = n; // tcnt存储当前有几棵树
+	for (int i = 0; i < m; ++i) {
+		if (tcnt == k) return cost; // 有k棵树时返回
+		if (find(edge[i].u) == find(edge[i].v)) continue; // 查询是否在同一棵树上
+		merge(edge[i].u, edge[i].v); // 合并两集合
+		cost += edge[i].w; // 合并的费用
+		tcnt--; // 每合并一次树的数量减一
+	}
+	return -1; // 无解
+}
+
+signed main() {
+	cin >> n >> m >> k;
+	init(n); // 初始化
+	for (int i = 0; i < m; ++i) {
+		cin >> edge[ecnt].u >> edge[ecnt].v >> edge[ecnt].w;
+		ecnt++; // 添加边
+	}
+	sort(edge, edge + m, cmp); // 先按边权排序
+	ll ans = kruskal();
+	if (ans == -1) cout << "No Answer\n";
+	else cout << ans << "\n";
+	return 0;
 }
