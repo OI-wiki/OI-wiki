1ee47f1c5a0982ea659c5f9ff5fe4dcbb4afda32
Tifa <62847935+Tiphereth-A@users.noreply.github.com>
2025-08-31 01:11:47 +0800

    refactor: simplify

--- scripts/fix-details.py
+++ scripts/fix-details.py
@@ -1,78 +1,52 @@
 import os
 import sys
+from typing import Iterable
 
-TAB_WIDTH = 4
 SKIP_EXTNAME = '.skipdetails'
 
 
-def index_lfirst_neq(l, value):
+def index_lfirst_neq(l: Iterable, value):
     return next(idx for idx, v in enumerate(l) if v != value)
 
 
 def fix_details(md_content: str):
     lines = md_content.splitlines()
-    if not lines or lines[-1]:
-        lines.append('')
-    stack = [0]
     result = []
+    last_indent = 0
 
-    in_code_block, in_math_block = False, False
-    code_fence = ''
+    # Assume that adjacent lines with identical indentation belong to the same block, so blank lines should align with the indentation of the preceding line.
+    for line in lines:
+        if line.strip():  # non-blank line
+            last_indent = index_lfirst_neq(line, ' ')
+        else:
+            line = ' '*last_indent
+        result.append(line)
 
-    for i, line in enumerate(lines):
-        assert not in_math_block or not in_code_block
+    # make bot happy
+    # Align the blank line with the indentation of the succeding line if the indentation of succeding line is shorter
+    lines, result = result, []
+    last_indent = 0
+    for line in reversed(lines):
+        if line.strip():  # non-blank line
+            last_indent = index_lfirst_neq(line, ' ')
+        elif len(line) > last_indent:
+            line = ' '*last_indent
+        result.append(line)
+    result = reversed(result)
 
-        trimmed = line.strip()
-
-        if not in_math_block and trimmed.startswith('```'):
-            if not code_fence:
-                code_fence = '`' * index_lfirst_neq(trimmed, '`')
-                in_code_block = not in_code_block
-                result.append(line)
-                continue
-            elif trimmed.startswith(code_fence):
-                code_fence = ''
-                in_code_block = not in_code_block
-                result.append(line)
-                continue
-
-        if not in_code_block and trimmed == '$$':
-            in_math_block = not in_math_block
-            result.append(line)
-            continue
-
-        if in_code_block or in_math_block:
-            result.append(line)
-            continue
-
-        if trimmed.startswith(('???', '!!!', '===')):
-            stack.append(index_lfirst_neq(line, ' ') + TAB_WIDTH)
-            result.append(line)
-            continue
-
-        try:
-            indent_len = index_lfirst_neq(
-                lines[index_lfirst_neq((l.strip() for l in lines[i:]), '')+i], ' ')
-            while stack and indent_len < stack[-1]:
-                stack.pop()
-        except StopIteration:
-            pass
-
-        result.append(line if trimmed else ' ' * stack[-1])
-
-    return '\n'.join(result)
+    return '\n'.join(result)+'\n'
 
 
-def check(filename):
-    if os.path.exists(f"{filename}{SKIP_EXTNAME}"):
+def check(filename: str):
+    if os.path.exists(filename + SKIP_EXTNAME):
         return
 
     old, new = '', ''
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         old = f.read()
         new = fix_details(old)
     if old != new:
-        with open(filename, "w", encoding="utf-8") as f:
+        with open(filename, 'w', encoding='utf-8') as f:
             f.write(new)
 
 
