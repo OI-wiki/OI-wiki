51687057b3bff8977e7920eeb08b0babc2a4e47b
Heran Yang <heran55@126.com>
2025-09-21 18:51:16 +0800

    feat: use vector as underlying container

--- docs/ds/code/queue/queue_3.cpp
+++ docs/ds/code/queue/queue_3.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -5,30 +6,29 @@ using namespace std;
 const int M = 1000000 + 5;
 
 struct Deque {
-  // 这里使用 vector 来模拟栈以减少空间常数
-  vector<int> f, s;
+  stack<int, vector<int>> f, s;
 
   bool empty() { return f.empty() && s.empty(); }
 
-  void push_back(int x) { f.push_back(x); }
+  void push_back(int x) { f.push(x); }
 
-  void push_front(int x) { s.push_back(x); }
+  void push_front(int x) { s.push(x); }
 
   void balance() {
     // 平衡中需要辅助栈实现栈内元素倒置
-    vector<int> t;
+    stack<int, vector<int>> t;
     if (s.empty()) {
       int n = f.size() / 2;
-      for (; f.size() > n; f.pop_back()) t.push_back(f.back());
-      for (; !t.empty(); t.pop_back()) s.push_back(t.back());
-      for (; !f.empty(); f.pop_back()) t.push_back(f.back());
+      for (; f.size() > n; f.pop()) t.push(f.top());
+      for (; !t.empty(); t.pop()) s.push(t.top());
+      for (; !f.empty(); f.pop()) t.push(f.top());
       f.swap(t);
       if (!f.empty()) s.swap(f);
     } else if (f.empty()) {
       int n = s.size() / 2;
-      for (; s.size() > n; s.pop_back()) t.push_back(s.back());
-      for (; !t.empty(); t.pop_back()) f.push_back(t.back());
-      for (; !s.empty(); s.pop_back()) t.push_back(s.back());
+      for (; s.size() > n; s.pop()) t.push(s.top());
+      for (; !t.empty(); t.pop()) f.push(t.top());
+      for (; !s.empty(); s.pop()) t.push(s.top());
       s.swap(t);
       if (!s.empty()) f.swap(s);
     }
@@ -36,22 +36,22 @@ struct Deque {
 
   void pop_front() {
     if (s.empty()) balance();
-    s.pop_back();
+    s.pop();
   }
 
   void pop_back() {
     if (f.empty()) balance();
-    f.pop_back();
+    f.pop();
   }
 
   int front() {
     if (s.empty()) balance();
-    return s.back();
+    return s.top();
   }
 
   int back() {
     if (f.empty()) balance();
-    return f.back();
+    return f.top();
   }
 
   int size() { return f.size() + s.size(); }
