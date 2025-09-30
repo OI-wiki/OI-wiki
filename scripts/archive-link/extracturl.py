8619a38b4403f425ad72aca0e8e41b5f6ece03c9
jyeric <jyeric1@126.com>
2025-09-30 12:51:16 +0800

    Refactor(extracturl.py): Load markdown extensions from mkdocs.yml instead of hardcoding

--- scripts/archive-link/extracturl.py
+++ scripts/archive-link/extracturl.py
@@ -1,25 +1,24 @@
 import markdown
 from bs4 import BeautifulSoup
+import yaml
+import re
 
 # extension list should be align with mkdocs's extension list
 
-md = markdown.Markdown(
-    extensions=['admonition', 'def_list', 'footnotes', 'meta', 'toc', 
-                'pymdownx.arithmatex', 'pymdownx.caret', 'pymdownx.critic',
-                'pymdownx.details', 'pymdownx.emoji', 'pymdownx.highlight', 
-                'pymdownx.inlinehilite', 'pymdownx.keys', 'pymdownx.magiclink',
-                'pymdownx.mark', 'pymdownx.snippets', 'pymdownx.progressbar',
-                'pymdownx.smartsymbols', 'pymdownx.superfences', 'pymdownx.tasklist',
-                'pymdownx.tilde', 'pymdownx.tabbed'])
+with open('mkdocs.yml', mode='r', encoding='utf-8') as f:
+    s = f.read()
 
+s = re.sub(r'!!python/name:([^\s]+)', r'"\1"', s)
+data = yaml.safe_load(s)
+extensions = []
+for d in data['markdown_extensions']:
+    if isinstance(d, str):
+        extensions.append(d)
+    elif isinstance(d, dict):
+        extensions = extensions + list(d.keys())
 
 def extract_links(md_text):
-    html = markdown.markdown(md_text, extensions=['admonition', 'def_list', 'footnotes', 'meta', 'toc', 
-                'pymdownx.arithmatex', 'pymdownx.caret', 'pymdownx.critic', 'pymdownx.details', 
-                'pymdownx.emoji', 'pymdownx.highlight', 'pymdownx.inlinehilite', 'pymdownx.keys',
-                'pymdownx.magiclink', 'pymdownx.mark', 'pymdownx.snippets', 'pymdownx.progressbar',
-                'pymdownx.smartsymbols', 'pymdownx.superfences', 'pymdownx.tasklist',
-                'pymdownx.tilde', 'pymdownx.tabbed'])
+    html = markdown.markdown(md_text, extensions=extensions)
     soup = BeautifulSoup(html, 'html.parser')
     links = []
     for a in soup.find_all('a'):
