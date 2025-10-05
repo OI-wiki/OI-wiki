3eb441b88c42460554b64148cc2a5c7b9b649142
Claudy Forrest <41589268+c-forrest@users.noreply.github.com>
2025-10-05 14:09:10 -0500

    fix: apply review suggestions

--- docs/dp/code/basic/lis-1.py
+++ docs/dp/code/basic/lis-1.py
@@ -1,8 +1,10 @@
 # --8<-- [start:core]
 def dp(n, a):
-    d = [1] * (n + 1)
+    d = [0] * (n + 1)
+    d[1] = 1
     ans = 1
     for i in range(2, n + 1):
+        d[i] = 1
         for j in range(1, i):
             if a[j] <= a[i]:
                 d[i] = max(d[i], d[j] + 1)
