f44e9fa9a803a70c7d042597881df697604c2c0a
smkttl <3121768621@qq.com>
2025-10-07 16:39:53 +0800

    update radix-sort codes

--- docs/basic/code/radix-sort/radix-sort_1.cpp
+++ docs/basic/code/radix-sort/radix-sort_1.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 using namespace std;
 // --8<-- [start:core]
-constexpr int MAXN = 1000;  // 要排序的数的个数
-constexpr int RADIX = 10;   // 基数
-constexpr int powRADIX[10] = {1,         10,        100,     1000,
+constexpr unsigned MAXN = 1000;  // 要排序的数的个数
+constexpr unsigned RADIX = 10;   // 基数
+constexpr unsigned powRADIX[10] = {1,         10,        100,     1000,
                               10000,     100000,    1000000, 10000000,
                               100000000, 1000000000};  // RADIX 的幂
-
-void MSD_radix_sort(int* begin, int* end, int digit)
+unsigned get_digit(unsigned value,int digit)// 提取第 digit 位
+{
+  return (value / powRADIX[digit]) % 10;
+}
+void MSD_radix_sort(unsigned* begin, unsigned* end, int digit)
 // 表示现在 [begin,end) 内的元素（10 进制下）前若干位都相同
 // 只有最后 digit 位（第 digit-1 到 0 位）需要进行排序
 // 调用示例：MSD_radix_sort(a,a+n,9)
@@ -17,19 +20,19 @@ void MSD_radix_sort(int* begin, int* end, int digit)
   {
     return;
   }
-  /**计数排序**/
-  static int cnt[RADIX + 1],
+  /**计数排序（个人写法仅供参考）**/
+  static unsigned cnt[RADIX + 1],
       tmp[MAXN + 5];  // 由于不同层递归不会同时使用 cnt,tmp
                       // 数组（每层使用完毕才会调用下一层），使用 static
                       // 关键字可以节省空间
-  vector<int> beg;
+  vector<unsigned> beg;
   beg.resize(RADIX + 1);  // 对 beg 的访问可能冲突，使用局部变量
   for (int i = 0; i <= RADIX; i++) {
     cnt[i] = beg[i] = 0;  // 清空是一个好习惯
   }
-  for (int* it = begin; it != end; it++)  // 计数
+  for (unsigned* it = begin; it != end; it++)  // 计数
   {
-    int bitVal = ((*it) / powRADIX[digit - 1]) % 10;  // 提取第 bit-1 位
+    int bitVal = get_digit(*it,digit-1);  
     cnt[bitVal] += 1;
   }
   beg[0] = 0;  // 计算每个数码开始存储的位置（偏移量）
@@ -41,15 +44,15 @@ void MSD_radix_sort(int* begin, int* end, int digit)
   for (int i = 0; i < RADIX; i++) {
     cnt[i] = 0;
   }
-  for (int* it = begin; it != end; it++)  // 将计数排序结果放入 tmp
+  for (unsigned* it = begin; it != end; it++)  // 将计数排序结果放入 tmp
   {
-    int bitVal = ((*it) / powRADIX[digit - 1]) % 10;  // 提取第 bit-1 位
+    unsigned bitVal = get_digit(*it,digit-1);  // 提取第 bit-1 位
     tmp[beg[bitVal] + cnt[bitVal]] =
         *it;  // 因为是倒序枚举，所以当前是第 cnt[bitVal]+1 个第 bit-1 位是
               // cnt[bitVal] 的
     cnt[bitVal]++;
   }
-  for (int* it = begin; it != end; it++)  // 将 tmp 拷贝回原数组
+  for (unsigned* it = begin; it != end; it++)  // 将 tmp 拷贝回原数组
   {
     *it = tmp[it - begin];
   }
@@ -65,7 +68,7 @@ void MSD_radix_sort(int* begin, int* end, int digit)
 }
 
 // --8<-- [end:core]
-int a[MAXN + 5];
+unsigned a[MAXN + 5];
 
 int main() {
   int n;
@@ -74,3 +77,4 @@ int main() {
   MSD_radix_sort(a + 1, a + n + 1, 9);
   for (int i = 1; i <= n; i++) cout << a[i] << " \n"[i == n];
 }
+
--- docs/basic/radix-sort.md
+++ docs/basic/radix-sort.md
@@ -125,41 +125,7 @@ $$
 下面是使用 LSD 基数排序实现的对 k - 关键字元素的排序。
 
 ```cpp
-constexpr int N = 100010;
-constexpr int W = 100010;
-constexpr int K = 100;
-
-int n, w[K], k, cnt[W];
-
-struct Element {
-  int key[K];
-
-  bool operator<(const Element& y) const {
-    // 两个元素的比较流程
-    for (int i = 1; i <= k; ++i) {
-      if (key[i] == y.key[i]) continue;
-      return key[i] < y.key[i];
-    }
-    return false;
-  }
-} a[N], b[N];
-
-void counting_sort(int p) {
-  memset(cnt, 0, sizeof(cnt));
-  for (int i = 1; i <= n; ++i) ++cnt[a[i].key[p]];
-  for (int i = 1; i <= w[p]; ++i) cnt[i] += cnt[i - 1];
-  // 为保证排序的稳定性，此处循环i应从n到1
-  // 即当两元素关键字的值相同时，原先排在后面的元素在排序后仍应排在后面
-  for (int i = n; i >= 1; --i) b[cnt[a[i].key[p]]--] = a[i];
-  memcpy(a, b, sizeof(a));
-}
-
-void radix_sort() {
-  for (int i = k; i >= 1; --i) {
-    // 借助计数排序完成对关键字的排序
-    counting_sort(i);
-  }
-}
+--8<-- "docs/basic/code/radix-sort/radix-sort_lsd.cpp:core"
 ```
 
 实际上并非必须从后往前枚举才是稳定排序，只需对 `cnt` 数组进行等价于 `std::exclusive_scan` 的操作即可。
