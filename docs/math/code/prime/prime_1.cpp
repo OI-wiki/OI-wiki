cd394ffe9e92dc46de3d240ee6d7d7cce0b669e7
sun2snow <77017505@qq.com>
2025-10-26 16:35:06 +0800

    Refactor formatting and spacing in prime_1.cpp

--- docs/math/code/prime/prime_1.cpp
+++ docs/math/code/prime/prime_1.cpp
@@ -13,17 +13,17 @@ lint ans = 2e18;
 // cnt：当前数值的因数个数
 // pre：上一个因子的幂次，限定本次选择的幂次
 void dfs(int u, lint num, lint cnt, int pre) {
-  if (cnt > n || u >= 15) return;
-  if (cnt == n) return ans = std::min(ans, num), void();
-  for (int i = 1; i <= pre; ++i) {
-    if (num * p[u] > ans) break;  // 剪枝
-    dfs(u + 1, num *= p[u], cnt * (i + 1), i);
-  }
+	if (cnt > n || u >= 15) return ;
+	if (cnt == n) return ans = std::min(ans, num), void();
+	for (int i = 1; i <= pre; ++i) { 
+		if (num * p[u] > ans) break; // 剪枝
+		dfs(u + 1, num *= p[u], cnt * (i + 1), i);
+	}
 }
 
 int main() {
-  std::cin >> n;
-  dfs(0, 1, 1, 60);  // ceil(log2(1e18))=60
-  std::cout << ans << std::endl;
-  return 0;
+	std::cin >> n;
+	dfs(0, 1, 1, 60); // ceil(log2(1e18))=60
+	std::cout << ans << std::endl;
+	return 0;
 }
