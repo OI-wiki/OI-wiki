130de2b24c8e586aff73270c209e374bce9fa4e0
Untitled_unrevised <348070954@qq.com>
2026-05-02 06:00:58 +0800

    fix(docs/misc/code/random/random_8.cpp): 修正代码错误

--- docs/misc/code/random/random_8.cpp
+++ docs/misc/code/random/random_8.cpp
@@ -8,14 +8,14 @@
 using u64 = unsigned long long;
 
 struct MyHash {
-  u64 mix64(u64 x) {
+  u64 mix64(u64 x) const {
     // x += 0x9e3779b97f4a7c15; // 这一步实际上失去意义
     x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
     x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
     return x ^ (x >> 31);
   }
 
-  size_t operator()(size_t x) {
+  size_t operator()(size_t x) const {
     static const u64 FIXED_RANDOM =
         std::chrono::steady_clock::now().time_since_epoch().count();
     return mix64(x + FIXED_RANDOM);
